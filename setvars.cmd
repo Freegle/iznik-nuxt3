d06131405f6a140beea33f20f29f7ba833dc4d64
Edward Hibbert <edward@ehibbert.org.uk>
2022-04-03 08:25:23 +0100

    Initial commit

---
+++ setvars.cmd
@@ -0,0 +1,5 @@
+set path=%HOMEDRIVE%%HOMEPATH%\AppData\Roaming\nvm\v16.13.2\;%path%
+set IZNIK_API=https://fdapidbg.ilovefreegle.org
+set API=http://fdapidbg.ilovefreegle.org/api
+set NODE_OPTIONS=--max_old_space_size=6000
+
