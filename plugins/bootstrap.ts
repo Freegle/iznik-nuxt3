ea6c6cfadb778d672212078faa716aeef2a0911c
Edward Hibbert <edward@ehibbert.org.uk>
2022-04-28 09:12:38 +0100

    WIP Azure deploy.

--- plugins/bootstrap.ts
+++ plugins/bootstrap.ts
@@ -1,13 +1,6 @@
-import BButton from 'bootstrap-vue-3'
-import BNotice from 'bootstrap-vue-3'
-import BNav from 'bootstrap-vue-3'
-import BNavBar from 'bootstrap-vue-3'
-import BNavItem from 'bootstrap-vue-3'
+// TODO Switch to new modules format for tree shaking.
+import BootstrapVue3 from 'bootstrap-vue-3'
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(BButton)
-  nuxtApp.vueApp.use(BNotice)
-  nuxtApp.vueApp.use(BNav)
-  nuxtApp.vueApp.use(BNavBar)
-  nuxtApp.vueApp.use(BNavItem)
+  nuxtApp.vueApp.use(BootstrapVue3)
 })
