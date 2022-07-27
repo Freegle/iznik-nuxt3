4ad96eff8b02db8afcd6f631b25944cbb335a097
Edward Hibbert <edward@ehibbert.org.uk>
2022-07-27 21:54:27 +0100

    Add custom error page.

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,11 +10,19 @@ module.exports = {
     'no-console': 'off',
     // no-v-model-argument rule is broken for Vue3, which requires that syntax for .sync.
     'vue/no-v-model-argument': 'off',
+    'vue/multi-word-component-names': [
+      'error',
+      {
+        // error page for custom errors triggers this, so ignore it.
+        ignores: ['error'],
+      },
+    ],
   },
   overrides: [
     {
       files: ['layouts/*.vue', 'pages/**/*.vue'],
       rules: {
+        // Not sure why we have two copies of this, one above and one here, but this one makes [id] work.
         'vue/multi-word-component-names': 'off',
       },
     },
