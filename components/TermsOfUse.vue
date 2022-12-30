d3937ede00ff0d965b598150f2c3282b3439d806
Edward Hibbert <edward@ehibbert.org.uk>
2022-12-30 16:49:42 +0000

    Disclaimer/privacy/terms pages.

---
+++ components/TermsOfUse.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <p>We could give you a ten page long Terms of Use. Like you'd read that.</p>
+    <p>As far as we're concerned, it's simple:</p>
+    <ol class="">
+      <li>
+        Use Freegle to pass on unwanted items direct to others. No sales, no
+        swaps, just simple gifts.
+      </li>
+      <li>Everything must be free and legal.</li>
+      <li>Be nice to other freeglers, and they'll be nice to you.</li>
+      <li>
+        <!-- eslint-disable-next-line-->
+        We don't see or check items offered - see our <nuxt-link to="/disclaimer">disclaimer</nuxt-link>.
+      </li>
+      <li>
+        Freegle communities may have additional rules. If you have questions,
+        scroll down and use the
+        <em>Contact community volunteers</em> button.
+      </li>
+      <li>
+        <!-- eslint-disable-next-line-->
+        Please <nuxt-link to="/disclaimer">keep yourself safe and watch out for scams</nuxt-link>.
+      </li>
+      <li>Objectionable content or behaviour will not be tolerated.</li>
+      <li>
+        If you see anything inappropriate, suspicious or just dodgy then please
+        <nuxt-link to="/help"> report it </nuxt-link>.
+      </li>
+      <li>You must be aged 13 or over, as there is user-generated content.</li>
+      <li>
+        <!-- eslint-disable-next-line-->
+        We won’t sell your email address or personal data to anyone else - see <nuxt-link to="/privacy">privacy page</nuxt-link>.
+      </li>
+      <li>
+        <!-- eslint-disable-next-line-->
+        We will email you posts, replies, events, newsletters, etc. - control these in <nuxt-link to="/settings">Settings</nuxt-link>.
+      </li>
+      <li>
+        <!-- eslint-disable-next-line-->
+        We will make your post public to publicise Freegle, but not your email address - see <nuxt-link to="/privacy">privacy page</nuxt-link>.
+      </li>
+    </ol>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {}
+  },
+}
+</script>
---
+++ pages/terms.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="d-flex justify-content-center">
+    <div class="terms__container px-3 bg-white">
+      <h1 class="text-center">Terms of Use</h1>
+      <TermsOfUse />
+      <h3>Got questions?</h3>
+      <p>
+        If you have questions, please
+        <nuxt-link to="/help"> contact us. </nuxt-link>
+      </p>
+    </div>
+  </div>
+</template>
+<script>
+import { buildHead } from '~/composables/useBuildHead'
+import TermsOfUse from '~/components/TermsOfUse'
+
+export default {
+  components: {
+    TermsOfUse,
+  },
+  setup() {
+    useHead(
+      buildHead('Terms of Use', "Our Terms of Use.  They're pleasingly short.")
+    )
+  },
+}
+</script>
+<style scoped lang="scss">
+@import '~bootstrap/scss/functions';
+@import '~bootstrap/scss/variables';
+@import '~bootstrap/scss/mixins/_breakpoints';
+
+.terms__container {
+  width: 100%;
+
+  @include media-breakpoint-up(md) {
+    width: 50%;
+  }
+}
+</style>
