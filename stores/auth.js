79fae3595a922a70790fc45bdb279f9bbddcf400
Edward Hibbert <edward@ehibbert.org.uk>
2023-02-08 18:07:16 +0000

    Persisting of auth wrong.

--- stores/auth.js
+++ stores/auth.js
@@ -18,13 +18,7 @@ export const useAuthStore = defineStore({
 
               // We don't persist much about the user, to avoid data getting 'stuck'.  All we need is enough to log us
               // in, and information about which users have been used on this device.
-              paths: [
-                'jwt',
-                'persistent',
-                'userlist',
-                'loginCount',
-                'loggedInEver',
-              ],
+              paths: ['auth', 'userlist', 'loginCount', 'loggedInEver'],
             },
           ],
   },
