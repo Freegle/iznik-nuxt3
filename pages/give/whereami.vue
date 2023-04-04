5dfb6c8491cdfd74afb94cb0dd99168445da24f3
Edward Hibbert <edward@ehibbert.org.uk>
2023-04-04 12:56:46 +0100

    Improve layout for give/find on mobile.

--- pages/give/whereami.vue
+++ pages/give/whereami.vue
@@ -1,100 +1,84 @@
 <template>
   <client-only>
-    <b-row class="m-0">
-      <b-col cols="12" lg="8" offset-lg="2" class="p-0 fader">
-        <GlobalWarning />
-        <client-only>
-          <WizardProgress :active-stage="2" class="d-none d-md-flex" />
-        </client-only>
-        <h1 class="text-center">Now, tell us where you are</h1>
-        <p class="text-center">
-          We'll use this to show your offer to people nearby. Don't worry, we
-          won't give other people your postcode.
-        </p>
-        <client-only>
-          <b-row>
-            <b-col class="text-center">
-              <PostCode
-                class="justify-content-center"
-                :value="initialPostcode"
-                @selected="postcodeSelect"
-                @cleared="postcodeClear"
-              />
-            </b-col>
-          </b-row>
-          <b-row v-if="!closed && postcodeValid">
-            <b-col class="text-center">
-              <transition name="fade">
-                <nuxt-link no-prefetch to="/give/whoami">
-                  <v-icon
-                    icon="check-circle"
-                    class="text-success mt-2 fa-bh"
-                    scale="5"
-                  />
-                </nuxt-link>
-              </transition>
-            </b-col>
-          </b-row>
-          <div v-if="postcodeValid && noGroups">
-            <NoticeMessage variant="info" class="mt-2">
-              We're really sorry, but there are no communities near there. If
-              you'd like to start one, please
-              <ExternalLink href="mailto:newgroups@ilovefreegle.org">
-                get in touch!
-              </ExternalLink>
-            </NoticeMessage>
-          </div>
-          <div v-else>
-            <b-row v-if="postcodeValid" class="mt-2">
-              <b-col class="text-center">
-                Freegle has local communities for each area. We'll show your
-                offer on this community first:
-              </b-col>
-            </b-row>
-            <b-row v-if="postcodeValid" class="mt-1">
-              <b-col class="d-flex justify-content-around">
-                <ComposeGroup />
-              </b-col>
-            </b-row>
-            <b-row v-if="postcodeValid" class="mt-1">
-              <b-col class="text-center text-muted small">
-                Click on the name above to choose a different community.
-              </b-col>
-            </b-row>
-          </div>
-          <div class="mt-1 d-block d-md-none">
-            <b-button
-              variant="primary"
-              size="lg"
-              block
-              class="w-100"
-              to="/give/whoami"
-            >
-              Next <v-icon icon="angle-double-right" />
-            </b-button>
-          </div>
-          <div class="mt-1 d-none d-md-flex justify-content-between">
-            <b-button
-              variant="secondary"
-              size="lg"
-              to="/give"
-              class="d-none d-md-block"
-            >
-              <v-icon icon="angle-double-left" /> Back
-            </b-button>
-            <b-button
-              v-if="postcodeValid && !closed"
-              variant="primary"
-              size="lg"
-              to="/give/whoami"
-            >
-              Next <v-icon icon="angle-double-right" />
-            </b-button>
-          </div>
-        </client-only>
-      </b-col>
-      <b-col cols="0" md="3" />
-    </b-row>
+    <div class="layout fader">
+      <GlobalWarning />
+      <WizardProgress :active-stage="2" class="d-none d-md-flex" />
+      <h1 class="text-center">Now, tell us where you are</h1>
+      <p class="text-center">
+        We'll use this to show your offer to people nearby. Don't worry, we
+        won't give other people your postcode.
+      </p>
+      <PostCode
+        class="justify-content-center"
+        :value="initialPostcode"
+        @selected="postcodeSelect"
+        @cleared="postcodeClear"
+      />
+      <div v-if="!closed && postcodeValid" class="text-center">
+        <transition name="fade">
+          <nuxt-link no-prefetch to="/give/whoami">
+            <v-icon
+              icon="check-circle"
+              class="text-success mt-2 fa-bh"
+              scale="5"
+            />
+          </nuxt-link>
+        </transition>
+      </div>
+      <div v-if="postcodeValid && noGroups">
+        <NoticeMessage variant="info" class="mt-2">
+          We're really sorry, but there are no communities near there. If you'd
+          like to start one, please
+          <ExternalLink href="mailto:newgroups@ilovefreegle.org">
+            get in touch!
+          </ExternalLink>
+        </NoticeMessage>
+      </div>
+      <div v-else>
+        <div v-if="postcodeValid" class="mt-2 text-center">
+          Freegle has local communities for each area. We'll show your offer on
+          this community first:
+        </div>
+        <div v-if="postcodeValid" class="mt-1 d-flex justify-content-around">
+          <ComposeGroup />
+        </div>
+        <div v-if="postcodeValid" class="mt-1 text-center text-muted small">
+          Click on the name above to choose a different community.
+        </div>
+      </div>
+      <div class="d-block d-md-none flex-grow-1" />
+      <div class="mt-1 d-block d-md-none">
+        <b-button
+          variant="primary"
+          size="lg"
+          block
+          class="w-100"
+          to="/give/whoami"
+        >
+          Next <v-icon icon="angle-double-right" />
+        </b-button>
+      </div>
+      <div class="w-100 d-flex justify-content-around">
+        <div class="mt-2 d-none d-md-flex justify-content-between maxbutt">
+          <b-button
+            variant="secondary"
+            size="lg"
+            to="/give"
+            class="d-none d-md-block"
+          >
+            <v-icon icon="angle-double-left" /> Back
+          </b-button>
+          <b-button
+            v-if="postcodeValid && !closed"
+            variant="primary"
+            size="lg"
+            to="/give/whoami"
+          >
+            Next <v-icon icon="angle-double-right" />
+          </b-button>
+        </div>
+      </div>
+    </div>
   </client-only>
 </template>
 <script>
@@ -151,8 +135,11 @@ export default {
   },
 }
 </script>
+<style scoped lang="scss">
+@import '~bootstrap/scss/functions';
+@import '~bootstrap/scss/variables';
+@import '~bootstrap/scss/mixins/_breakpoints';
 
-<style scoped>
 select {
   max-width: 400px !important;
 }
@@ -161,5 +148,19 @@ select {
   background-color: rgba(246, 246, 236, 0.6);
   box-shadow: 0 0 80px 450px rgba(246, 246, 236, 0.6);
   font-weight: bold;
+}
+
+@include media-breakpoint-down(md) {
+  .layout {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    padding-bottom: 84px;
+  }
+}
+
+.maxbutt {
+  width: 33vw;
 }
 </style>
