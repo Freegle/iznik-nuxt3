ce68b5a87f8572780e813042fda2485236749dd9
edwh <edward@ehibbert.org.uk>
2023-04-12 11:16:34 +0100

    Re-enable Sentry.

--- plugins/sentry.client.ts
+++ plugins/sentry.client.ts
@@ -6,43 +6,39 @@ export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig()
   const { vueApp } = nuxtApp
 
-  const test = 1
-
-  if (test > 2) {
-    Sentry.init({
-      app: [vueApp],
-      dsn: config.SENTRY_DSN,
-      // Some errors seem benign, and so we ignore them on the client side rather than clutter our sentry logs.
-      ignoreErrors: [
-        'ResizeObserver loop limit exceeded', // Benign - see https://stackoverflow.com/questions/49384120/resizeobserver-loop-limit-exceeded
-        'Navigation cancelled from ', // This can happen if someone clicks twice in quick succession
-      ],
-      integrations: [
-        new Integrations.BrowserTracing({
-          routingInstrumentation: Sentry.vueRouterInstrumentation(
-            nuxtApp.$router
-          ),
-          tracingOrigins: ['localhost', 'ilovefreegle.org', /^\//],
-        }),
-      ],
-      logErrors: false, // Note that this doesn't seem to work with nuxt 3
-      tracesSampleRate: config.SENTRY_TRACES_SAMPLE_RATE || 1.0, // Sentry recommends adjusting this value in production
-      debug: config.SENTRY_ENABLE_DEBUG || false, // Enable debug mode
-      environment: config.ENVIRONMENT || 'dev', // Set environment
-      // The following enables exeptions to be logged to console despite logErrors being set to false (preventing them from being passed to the default Vue err handler)
-      beforeSend(event, hint) {
-        // Check if it is an exception, and if so, log it.
-        if (event.exception) {
-          console.error(
-            `[Exeption handled by Sentry]: (${hint.originalException})`,
-            { event, hint }
-          )
-        }
-        // Continue sending to Sentry
-        return event
-      },
-    })
-  }
+  Sentry.init({
+    app: [vueApp],
+    dsn: config.SENTRY_DSN,
+    // Some errors seem benign, and so we ignore them on the client side rather than clutter our sentry logs.
+    ignoreErrors: [
+      'ResizeObserver loop limit exceeded', // Benign - see https://stackoverflow.com/questions/49384120/resizeobserver-loop-limit-exceeded
+      'Navigation cancelled from ', // This can happen if someone clicks twice in quick succession
+    ],
+    integrations: [
+      new Integrations.BrowserTracing({
+        routingInstrumentation: Sentry.vueRouterInstrumentation(
+          nuxtApp.$router
+        ),
+        tracingOrigins: ['localhost', 'ilovefreegle.org', /^\//],
+      }),
+    ],
+    logErrors: false, // Note that this doesn't seem to work with nuxt 3
+    tracesSampleRate: config.SENTRY_TRACES_SAMPLE_RATE || 1.0, // Sentry recommends adjusting this value in production
+    debug: config.SENTRY_ENABLE_DEBUG || false, // Enable debug mode
+    environment: config.ENVIRONMENT || 'dev', // Set environment
+    // The following enables exeptions to be logged to console despite logErrors being set to false (preventing them from being passed to the default Vue err handler)
+    beforeSend(event, hint) {
+      // Check if it is an exception, and if so, log it.
+      if (event.exception) {
+        console.error(
+          `[Exeption handled by Sentry]: (${hint.originalException})`,
+          { event, hint }
+        )
+      }
+      // Continue sending to Sentry
+      return event
+    },
+  })
 
   vueApp.mixin(
     Sentry.createTracingMixins({
