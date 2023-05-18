695e0a859fbd33420d2b5fe0e353487f0509b3dd
Edward Hibbert <edward@ehibbert.org.uk>
2023-05-18 20:46:12 +0100

    Reload on all chunks, not just routes (as per https://github.com/nuxt/nuxt/pull/19086#issuecomment-1553529591)

---
+++ plugins/chunks.client.js
@@ -0,0 +1,21 @@
+import * as Sentry from '@sentry/browser'
+import { useRoute } from 'vue-router'
+import { defineNuxtPlugin } from '#app/nuxt'
+import { reloadNuxtApp } from '#app/composables/chunk'
+
+export default defineNuxtPlugin({
+  setup(nuxtApp) {
+    const route = useRoute()
+
+    nuxtApp.hook('app:chunkError', ({ error }) => {
+      Sentry.captureMessage(
+        'Caught chunk error in ' +
+          route.path +
+          ', will reload: ' +
+          JSON.stringify(error)
+      )
+
+      reloadNuxtApp({ path: route.path, persistState: true })
+    })
+  },
+})
