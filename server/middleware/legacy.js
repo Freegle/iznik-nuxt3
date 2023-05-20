e2c0b47f2f71961a1cfc579f7640ce4f917b304f
Edward Hibbert <edward@ehibbert.org.uk>
2023-05-20 11:55:13 +0100

    Ask apps to update.

--- server/middleware/legacy.js
+++ server/middleware/legacy.js
@@ -5,6 +5,34 @@ export default defineEventHandler((event) => {
   ) {
     console.log('Return 404 for: ' + event.node.req.url)
     setResponseStatus(event, 404)
+  } else if (event.node.req.url.includes('app_fd_version_android_required')) {
+    // We no longer have the API to return this value, so do it manually.
+    console.log('Ask Android app to update')
+    return {
+      ret: 0,
+      status: 'Success',
+      values: [
+        {
+          id: 14,
+          key: 'app_fd_version_android_required',
+          value: '2.0.120',
+        },
+      ],
+    }
+  } else if (event.node.req.url.includes('app_fd_version_ios_required')) {
+    // We no longer have the API to return this value, so do it manually.
+    console.log('Ask IOS app to update')
+    return {
+      ret: 0,
+      status: 'Success',
+      values: [
+        {
+          id: 14,
+          key: 'app_fd_version_ios_required',
+          value: '2.0.120',
+        },
+      ],
+    }
   } else if (event.node.req.url.indexOf('/api/') === 0) {
     // This is a request to the /api endpoint which was wrongly used by older clients.  Return a maintenance response
     // code which should stop them retrying.
