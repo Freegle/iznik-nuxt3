ed888d90aa0940bc915be9b9842a0e3c72fb7962
Edward Hibbert <edward@ehibbert.org.uk>
2023-05-20 13:53:49 +0100

    Suppress some pointless Sentry errors.

--- plugins/sentry.client.ts
+++ plugins/sentry.client.ts
@@ -45,6 +45,82 @@ export default defineNuxtPlugin((nuxtApp) => {
           { event, hint }
         )
       }
+
+      if (hint) {
+        console.log(
+          'Original exception was',
+          hint.originalException,
+          typeof hint.originalException
+        )
+
+        if (!hint.originalException) {
+          // There's basically no info to report, so there's nothing we can do.  Suppress it.
+          console.log('No info - suppress exception')
+          return null
+        } else if (
+          hint.originalException.toString().match(/Down for maintenance/)
+        ) {
+          console.log('Maintenance - suppress exception', this)
+          return null
+        } else if (
+          hint.originalException.name &&
+          hint.originalException.name === 'TypeError'
+        ) {
+          console.log('TypeError')
+          if (hint.originalException.message) {
+            console.log('Message', hint.originalException.message)
+
+            if (
+              hint.originalException.message.match(/leaflet/) ||
+              hint.originalException.message.match(/getPosition/)
+            ) {
+              // Leaflet produces all sorts of errors, which are not really our fault and don't affect the user.
+              console.log('Suppress leaflet exception')
+              return null
+            } else if (
+              hint.originalException.message.match(
+                /can't redefine non-configurable property "userAgent"/
+              )
+            ) {
+              // This exception happens a lot, and the best guess I can find is that it is a bugged browser
+              // extension.
+              console.log('Suppress userAgent')
+              return null
+            } else if (hint.originalException.message.match(/cancelled/)) {
+              // This probably happens due to the user changing their mind and navigating away immediately.
+              console.log('Suppress cancelled')
+              return null
+            }
+          }
+        } else if (
+          hint.originalException.name &&
+          hint.originalException.name === 'ReferenceError'
+        ) {
+          console.log('ReferenceError')
+          if (hint.originalException.message) {
+            console.log('Message', hint.originalException.message)
+
+            if (
+              hint.originalException.message.match(
+                /Can't find variable: fieldset/
+              )
+            ) {
+              // This happens because of an old bug which is now fixed:
+              // https://codereview.chromium.org/2343013005
+              console.log('Old Chrome fieldset bug')
+              return null
+            }
+          }
+        }
+      }
+
+      // Add some more detail if we can.
+      if (hint && hint.originalException instanceof Event) {
+        event.extra.isTrusted = hint.originalException.isTrusted
+        event.extra.detail = hint.originalException.detail
+        event.extra.type = hint.originalException.type
+      }
+
       // Continue sending to Sentry
       return event
     },
