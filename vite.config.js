16fa0b63c8cb8dc1f90c239df8444896b54888a1
Edward Hibbert <edward@ehibbert.org.uk>
2023-05-21 11:44:36 +0100

    Try switching to modern browser support to reduce load time.

--- vite.config.js
+++ vite.config.js
@@ -26,7 +26,7 @@ export default {
     // Make Lint errors cause build failures.
     eslintPlugin(),
     legacy({
-      // es2015 is the earliest supported by Vite, and is the default.
+      targets: ['> 0.5%, last 2 versions, Firefox ESR, not dead'],
     }),
   ],
 
