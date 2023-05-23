33cc15881a9becdb3d5bb63724bef580516c3e0b
Edward Hibbert <edward@ehibbert.org.uk>
2023-05-23 15:13:32 +0100

    Add CSP and reporting.

---
+++ server/middleware/csp.js
@@ -0,0 +1,6 @@
+export default defineEventHandler((event) => {
+  event.node.res.setHeader(
+    'Content-Security-Policy-Report-Only',
+    "object-src 'none'; script-src 'self' 'unsafe-inline' 'unsafe-eval' *.netlify.com *.facebook.net *.google.com; report-uri https://fdapilive.ilovefreegle.org/csp.php"
+  )
+})
