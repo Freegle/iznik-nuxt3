d671bb05dfc84bb2c9464061b05bef1299172914
Edward Hibbert <edward@ehibbert.org.uk>
2023-05-23 16:16:08 +0100

    Put CSP live.

--- server/middleware/csp.js
+++ server/middleware/csp.js
@@ -1,7 +1,7 @@
 // CSP.  See also _hedaers.
 export default defineEventHandler((event) => {
   event.node.res.setHeader(
-    'Content-Security-Policy-Report-Only',
+    'Content-Security-Policy',
     "object-src 'none'; worker-src: blob; script-src 'self' 'unsafe-inline' 'unsafe-eval' *.netlify.app *.facebook.net *.google.com cdn.jsdelivr.net *.paypalobjects.com; report-uri https://fdapilive.ilovefreegle.org/csp.php"
   )
 })
