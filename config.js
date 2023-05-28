648bf777de1b274bd7637a8fc21d881c120085c5
Edward Hibbert <edward@ehibbert.org.uk>
2023-05-28 15:36:37 +0100

    APIv2 is now hosted on api.ilovefreegle.org port 443 (as well as old 8192).  8192 is deprecated.

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ export default {
   APIv1: process.env.IZNIK_API_V1 || 'https://fdapilive.ilovefreegle.org/api',
 
   // New style API, for fast read ops.
-  APIv2: process.env.IZNIK_API_V2 || 'https://api.ilovefreegle.org:8192/apiv2',
+  APIv2: process.env.IZNIK_API_V2 || 'https://api.ilovefreegle.org/apiv2',
 
   // This is where the user site is.
   USER_SITE: 'https://www.ilovefreegle.org',
