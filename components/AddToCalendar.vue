5051a71c1ea166a91d1c908d7f1d49054a9dbe9f
Chris Cant <chris@phdcc.com>
2023-07-03 23:45:56 +0100

    Try app AddToCalendar options

--- components/AddToCalendar.vue
+++ components/AddToCalendar.vue
@@ -8,6 +8,7 @@
 </template>
 <script>
 import saveAs from 'save-file'
+//import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
 
 export default {
   props: {
@@ -29,11 +30,30 @@ export default {
   methods: {
     async download(e) {
       // https://www.npmjs.com/package/save-file
-      console.log("AddToCalendar download")
+      console.log("AddToCalendar download", typeof this.ics, this.ics.length)
       e.preventDefault()
       e.stopPropagation()
-      const blob = new Blob([this.ics], { type: 'text/calendar;charset=utf-8' })
-      await saveAs(blob, 'freegle-handover.ics')
+      // const blob = new Blob([this.ics], { type: 'text/calendar;charset=utf-8' })
+      // await saveAs(blob, 'freegle-handover.ics')
+      /*const blob = new Blob([this.ics], { type: 'application/octet-stream' })
+      const link = document.createElement('a')
+      link.href = URL.createObjectURL(blob)
+      link.download = 'freegle-handover.ics'
+      link.click()
+      URL.revokeObjectURL(link.href)*/
+
+      /*// https://capacitorjs.com/docs/apis/filesystem
+      await Filesystem.writeFile({
+        path: 'freegle-handover.ics',
+        data: this.ics,
+        directory: Directory.Documents,
+        encoding: Encoding.UTF8,
+      })*/
+
+      // webcal://localhost/myCalendar.ics
+      // https://www.npmjs.com/package/cordova-plugin-calendar
+
+      console.log("DONE!")
     },
   },
 }
