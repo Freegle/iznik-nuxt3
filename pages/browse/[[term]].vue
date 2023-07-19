14d40642a6aeded1164cd3450cdeccbef779719e
Edward Hibbert <edward@ehibbert.org.uk>
2023-07-19 11:13:53 +0100

    Sentry fix.

--- pages/browse/[[term]].vue
+++ pages/browse/[[term]].vue
@@ -228,7 +228,7 @@ export default {
         // Not asked too recently.
         await this.fetchMe(true)
 
-        if (this.me && (!this.me.aboutme || !this.me.aboutme.text)) {
+        if (!this.me.aboutme || !this.me.aboutme.text) {
           // We have not yet provided one.
           const daysago = dayjs().diff(dayjs(this.me.added), 'days')
 
