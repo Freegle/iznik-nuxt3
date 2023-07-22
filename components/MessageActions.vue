c15acba98395ae47ef53c1b8a92be692c198ea2b
Edward Hibbert <edward@ehibbert.org.uk>
2023-07-22 11:50:38 +0100

    Post report button doesn't work and shouldn't show when not logged in.

--- components/MessageActions.vue
+++ components/MessageActions.vue
@@ -2,7 +2,7 @@
   <div class="d-flex mt-1 justify-content-between">
     <div class="flex-grow-1">
       <b-button
-        v-if="message.groups && message.groups.length"
+        v-if="loggedIn && message.groups && message.groups.length"
         variant="link"
         class="grey p-0 mr-4"
         size="sm"
