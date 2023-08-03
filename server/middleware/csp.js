9f3b6844c996f53a64f80aa11f4331d0796015b7
Edward Hibbert <edward@ehibbert.org.uk>
2023-08-03 17:03:15 +0100

    Revert "Allow GTM script to load."
    This reverts commit eed9bcf3f3a2f96ec7755985db47445760cf9d3b.

--- server/middleware/csp.js
+++ server/middleware/csp.js
@@ -2,6 +2,6 @@
 export default defineEventHandler((event) => {
   event.node.res.setHeader(
     'Content-Security-Policy',
-    "object-src 'none'; worker-src blob:; script-src 'self' 'unsafe-inline' 'unsafe-eval' *.facebook.net *.google.com *.gstatic.com *.googletagmanager.com cdn.jsdelivr.net *.paypalobjects.com; report-uri https://fdapilive.ilovefreegle.org/csp.php"
+    "object-src 'none'; worker-src blob:; script-src 'self' 'unsafe-inline' 'unsafe-eval' *.facebook.net *.google.com *.gstatic.com cdn.jsdelivr.net *.paypalobjects.com; report-uri https://fdapilive.ilovefreegle.org/csp.php"
   )
 })
