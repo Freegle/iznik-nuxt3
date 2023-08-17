b3814e2a028523f7856be3bfa02086d38f753430
Matthew Mamonov <g.smellyshovel@gmail.com>
2023-08-17 19:52:06 +0400

    WIP on refactoring myposts.vue - move the OFFERs and WANTEDs lists into a separate component

--- pages/myposts.vue
+++ pages/myposts.vue
@@ -23,171 +23,21 @@
             <VisibleWhen :at="['xs', 'sm', 'md']">
               <JobsTopBar />
             </VisibleWhen>
-            <b-card
-              class="mt-2"
-              border-variant="info"
-              header="info"
-              header-bg-variant="info"
-              header-text-variant="white"
-              no-body
-            >
-              <template #header>
-                <div class="d-flex justify-content-between">
-                  <h2 class="d-inline header--size3">
-                    <v-icon icon="gift" scale="2" /> Your OFFERs
-                  </h2>
-                  <span v-if="oldOfferCount > 0">
-                    <span v-if="showOldOffers">
-                      <b-button
-                        variant="secondary"
-                        title="Show old OFFERs"
-                        @click="toggleOldOffers"
-                      >
-                        Hide {{ oldOfferCountStr }}
-                      </b-button>
-                    </span>
-                    <span v-else>
-                      <b-button variant="secondary" @click="toggleOldOffers">
-                        +{{ oldOfferCountStr }}
-                      </b-button>
-                    </span>
-                  </span>
-                </div>
-              </template>
-              <b-card-body class="p-1 p-lg-3">
-                <b-card-text class="text-center">
-                  <p v-if="activeOfferCount > 0" class="text-muted">
-                    Stuff you're giving away.
-                  </p>
-                  <b-img
-                    v-if="busyOffers && offers.length === 0"
-                    lazy
-                    src="/loader.gif"
-                    alt="Loading..."
-                    width="100px"
-                  />
-                  <div
-                    v-if="
-                      busyOffers ||
-                      activeOfferCount > 0 ||
-                      (showOldOffers && offers.length > 0)
-                    "
-                  >
-                    <div
-                      v-for="message in offersShown"
-                      :key="'message-' + message.id"
-                      class="p-0 text-start mt-1"
-                    >
-                      <MyMessage
-                        :id="message.id"
-                        :show-old="showOldOffers"
-                        :expand="expand"
-                      />
-                    </div>
-                    <InfiniteLoading
-                      :key="infiniteIdOffers"
-                      :distance="distance"
-                      @infinite="loadMoreOffers"
-                    />
-                  </div>
-                  <div v-else>
-                    <b-row>
-                      <b-col>
-                        <p>You have no active OFFERs.</p>
-                      </b-col>
-                    </b-row>
-                    <b-row>
-                      <b-col class="text-center">
-                        <b-button
-                          to="/give"
-                          class="mt-1"
-                          size="lg"
-                          variant="primary"
-                        >
-                          <v-icon icon="gift" />&nbsp;OFFER something
-                        </b-button>
-                      </b-col>
-                    </b-row>
-                  </div>
-                </b-card-text>
-              </b-card-body>
-            </b-card>
-            <b-card
-              class="mt-2"
-              border-variant="info"
-              header="info"
-              header-bg-variant="info"
-              header-text-variant="white"
-              no-body
-            >
-              <template #header>
-                <div class="d-flex justify-content-between">
-                  <h2 class="d-inline header--size3">
-                    <v-icon icon="shopping-cart" scale="2" /> Your WANTEDs
-                  </h2>
-                  <span v-if="oldWantedCount > 0">
-                    <span v-if="showOldWanteds">
-                      <b-button
-                        variant="secondary"
-                        title="Show old WANTEDs"
-                        @click="toggleOldWanteds"
-                      >
-                        Hide {{ oldWantedCountStr }}
-                      </b-button>
-                    </span>
-                    <span v-else>
-                      <b-button variant="secondary" @click="toggleOldWanteds">
-                        +{{ oldWantedCountStr }}
-                      </b-button>
-                    </span>
-                  </span>
-                </div>
-              </template>
-              <b-card-body class="p-1 p-lg-3">
-                <b-card-text class="text-center">
-                  <p v-if="activeWantedCount > 0" class="text-muted">
-                    Stuff you're trying to find.
-                  </p>
-                  <div
-                    v-if="
-                      busyWanteds ||
-                      activeWantedCount > 0 ||
-                      (showOldWanteds && wanteds.length > 0)
-                    "
-                  >
-                    <div
-                      v-for="message in wantedsShown"
-                      :key="'message-' + message.id"
-                      class="p-0 text-start mt-1"
-                    >
-                      <MyMessage
-                        :id="message.id"
-                        :show-old="showOldWanteds"
-                        :expand="expand"
-                      />
-                    </div>
-                    <infinite-loading
-                      :key="infiniteIdWanteds"
-                      :distance="distance"
-                      @infinite="loadMoreWanteds"
-                    />
-                  </div>
-                  <div v-else>
-                    <p>You have no active WANTEDs.</p>
-                    <div class="d-flex justify-content-around mb-2">
-                      <b-button
-                        to="/find"
-                        class="mt-1"
-                        size="lg"
-                        variant="secondary"
-                      >
-                        <v-icon icon="shopping-cart" />&nbsp;Ask for something
-                      </b-button>
-                    </div>
-                  </div>
-                </b-card-text>
-              </b-card-body>
-            </b-card>
+
+            <MyPostsPostsList
+              v-if="offers"
+              type="Offer"
+              :posts="offers"
+              @load-more="loadMoreOffers"
+            />
+
+            <MyPostsPostsList
+              v-if="wanteds"
+              type="Wanted"
+              :posts="wanteds"
+              @load-more="loadMoreWanteds"
+            />
+
             <b-card
               class="mt-2"
               border-variant="info"
@@ -247,7 +97,6 @@
 
 <script setup>
 import { useRoute } from 'vue-router'
-import pluralize from 'pluralize'
 import dayjs from 'dayjs'
 import { useAuthStore } from '../stores/auth'
 import { useMessageStore } from '../stores/message'
@@ -256,12 +105,11 @@ import { useSearchStore } from '../stores/search'
 import { buildHead } from '~/composables/useBuildHead'
 
 import VisibleWhen from '~/components/VisibleWhen'
-import InfiniteLoading from '~/components/InfiniteLoading'
 import SidebarLeft from '~/components/SidebarLeft'
 import SidebarRight from '~/components/SidebarRight'
 import ExpectedRepliesWarning from '~/components/ExpectedRepliesWarning'
 import JobsTopBar from '~/components/JobsTopBar'
-import MyMessage from '~/components/MyMessage.vue'
+import MyPostsPostsList from '~/components/MyPostsPostsList.vue'
 import UserSearch from '~/components/UserSearch.vue'
 import DonationAskModal from '~/components/DonationAskModal'
 
@@ -304,74 +152,15 @@ const askmodal = ref()
 
 const myid = authStore.user?.id
 
-let messages = []
+const messages = ref([])
 let expand = false
 
 if (myid) {
-  messages = await messageStore.fetchByUser(myid, false, true)
-  expand = messages.length <= 5
+  messages.value = await messageStore.fetchByUser(myid, false, true)
+  expand = messages.value.length <= 5
 
   // No need to wait for searches - often below the fold.
   searchStore.fetch(myid)
-}
-
-const distance = ref(1000)
-
-const busyOffers = ref(true)
-const showOldOffers = ref(false)
-
-const offers = computed(() => {
-  return (
-    messages
-      ?.filter(
-        (message) =>
-          message.type === 'Offer' &&
-          (showOldOffers.value || !message.hasoutcome)
-      )
-      .sort(sortPosts) ?? []
-  )
-})
-
-const infiniteIdOffers = ref(1)
-watch(offers, () => infiniteIdOffers.value++)
-
-const oldOfferCount = computed(() => {
-  return messages.filter(
-    (message) => message.type === 'Offer' && message.hasoutcome
-  ).length
-})
-
-const oldOfferCountStr = computed(() => {
-  return pluralize('old OFFER', oldOfferCount.value, true)
-})
-
-const activeOfferCount = computed(() => {
-  return messages.filter(
-    (message) => message.type === 'Offer' && !message.hasoutcome
-  ).length
-})
-
-const offersToShow = ref(0)
-
-const offersShown = computed(() => {
-  return offers.value.slice(0, offersToShow.value)
-})
-
-async function loadMoreOffers($state) {
-  offersToShow.value++
-
-  if (offersToShow.value > offers.value.length) {
-    offersToShow.value = offers.value.length
-    busyOffers.value = false
-    $state.complete()
-  } else {
-    await messageStore.fetch(offers.value[offersToShow.value - 1].id)
-    $state.loaded()
-  }
-}
-
-function toggleOldOffers() {
-  showOldOffers.value = !showOldOffers.value
 }
 
 const { $bus } = useNuxtApp()
@@ -412,75 +201,29 @@ onMounted(() => {
   })
 })
 
-const busyWanteds = ref(true)
-const showOldWanteds = ref(false)
-
-const wanteds = computed(() => {
-  return (
-    messages
-      ?.filter(
-        (message) =>
-          message.type === 'Wanted' &&
-          (showOldOffers.value || !message.hasoutcome)
-      )
-      .sort(sortPosts) ?? []
-  )
+const offers = computed(() => {
+  return messages.value.filter((message) => message.type === 'Offer')
 })
 
-const infiniteIdWanteds = ref(1)
-watch(wanteds, () => infiniteIdWanteds.value++)
-
-const oldWantedCount = computed(() => {
-  return messages.filter(
-    (message) => message.type === 'Wanted' && message.hasoutcome
-  ).length
-})
-
-const oldWantedCountStr = computed(() => {
-  return pluralize('old WANTED', oldWantedCount.value, true)
-})
-
-const activeWantedCount = computed(() => {
-  return messages.filter(
-    (message) => message.type === 'Wanted' && !message.hasoutcome
-  ).length
-})
-
-const wantedsToShow = ref(1)
-
-const wantedsShown = computed(() => {
-  return wanteds.value.slice(0, wantedsToShow.value)
-})
-
-async function loadMoreWanteds($state) {
-  wantedsToShow.value++
-
-  if (wantedsToShow.value > wanteds.value.length) {
-    wantedsToShow.value = wanteds.value.length
-    busyWanteds.value = false
-    $state.complete()
+async function loadMoreOffers($state) {
+  if (offers.value.length - 1 > offers.value.length) {
+    // $state.complete()
   } else {
-    await messageStore.fetch(wanteds.value[wantedsToShow.value - 1].id)
-    $state.loaded()
+    await messageStore.fetch(offers.value[offers.value.length - 1].id)
+    // $state.loaded()
   }
 }
 
-function toggleOldWanteds() {
-  showOldWanteds.value = !showOldWanteds.value
-}
+const wanteds = computed(() => {
+  return messages.value.filter((message) => message.type === 'Wanted')
+})
 
-function sortPosts(a, b) {
-  // Show promised items first, then by most recently posted.
-
-  const showOld =
-    a.type === 'Offer' ? showOldOffers.value : showOldWanteds.value
-
-  if (!showOld && a.promised && !b.promised) {
-    return -1
-  } else if (!showOld && b.promised && !a.promised) {
-    return 1
+async function loadMoreWanteds($state) {
+  if (wanteds.value.length - 1 > wanteds.value.length) {
+    // $state.complete()
   } else {
-    return new Date(b.arrival).getTime() - new Date(a.arrival).getTime()
+    await messageStore.fetch(wanteds.value[wanteds.value.length - 1].id)
+    // $state.loaded()
   }
 }
 
