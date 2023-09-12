85a6793a5cf12ba2c221ccd8a0c098de8e684688
Matthew Mamonov <g.smellyshovel@gmail.com>
2023-09-12 20:14:24 +0400

    Working useModel implementation; WIP on the navigation guards

--- composables/useModal.js
+++ composables/useModal.js
@@ -1,56 +1,18 @@
-import { nextTick, onUnmounted } from 'vue'
-import { useRouter } from 'vue-router'
+import { onUnmounted } from 'vue'
 
-export function useModal(props) {
-  // the modals are hidden by default
-  const show = ref(false)
+export function useModal() {
+  const modal = ref()
 
-  watch(
-    () => props.modelValue,
-    (value) => {
-      nextTick().then(() => {
-        show.value = value
-      })
-    }
-  )
-
-  // If the component has been instantiated with modelValue true, then there is an odd problem which means
-  // the modal animation doesn't work.  By using nextTick and a separate variable to control the modal
-  // show, we can make the modal animation work.
-  if (props.modelValue) {
-    nextTick().then(() => {
-      show.value = true
-    })
-  }
-
-  // Register a hook for routing so that we can close the modal rather than move route.  This is to handle the
-  // back button case, where you expect the back button to close the modal.
-  //
-  // This is a bit of a hack, but there doesn't seem to be a good way to do it.  If we have routed to a chat
-  // from within a modal then make sure we route to it - this is a case that happens if you Message someone from
-  // a modal on mobile.
-  const unregisterRouterGuard = useRouter().beforeEach((to, from, next) => {
-    if (!to.path.includes('/chats')) {
-      back(next)
-    } else {
-      // It's to a chat.
-      next()
-    }
+  onMounted(() => {
+    modal.value.show()
   })
 
-  function back(next) {
-    if (show.value) {
-      // Back should close the modal
-      show.value = false
-    } else {
-      // No modal open - proceed with back.
-      next()
-    }
-  }
-
-  onUnmounted(() => {
-    unregisterRouterGuard()
+  const unregisterNavigationGuard = useRouter().beforeEach((to, from, next) => {
+    // TODO
+    next()
   })
 
-  return { show }
+  onUnmounted(unregisterNavigationGuard)
+
+  return { modal }
 }
