5227b5b3efd73c40a7d43a7e05f6ada052d29f6d
Matthew Mamonov <g.smellyshovel@gmail.com>
2023-09-14 20:29:36 +0400

    VolunteerOpportunityModal.vue and its usage in [[groupid]].vue

--- components/VolunteerOpportunityModal.vue
+++ components/VolunteerOpportunityModal.vue
@@ -1,11 +1,5 @@
 <template>
-  <b-modal
-    id="profilemodal"
-    v-model="showModal"
-    scrollable
-    size="lg"
-    no-stacking
-  >
+  <b-modal ref="modal" scrollable size="lg" no-stacking>
     <template #header>
       <h4 v-if="added">Your opportunity has been added</h4>
       <h4 v-else-if="editing">
@@ -434,6 +428,7 @@ import SpinButton from '~/components/SpinButton.vue'
 import modal from '@/mixins/modal'
 import { twem } from '~/composables/useTwem'
 import { ref } from '#imports'
+import { useModal } from '~/composables/useModal'
 const GroupSelect = () => import('~/components/GroupSelect')
 const OurFilePond = () => import('~/components/OurFilePond')
 const StartEndCollection = () => import('~/components/StartEndCollection')
@@ -511,6 +506,11 @@ export default {
 
     const oldPhoto = ref(volunteeringStore.byId(props.id)?.image)
 
+    const { modal, hide } = useModal()
+
+    const editing = ref(props.startEdit)
+    const added = ref(false)
+
     return {
       volunteeringStore,
       composeStore,
@@ -518,12 +518,14 @@ export default {
       groupStore,
       groupid,
       oldPhoto,
+      modal,
+      hide,
+      editing,
+      added,
     }
   },
   data() {
     return {
-      editing: false,
-      added: false,
       cacheBust: Date.now(),
       uploading: false,
       showGroupError: false,
@@ -588,19 +590,6 @@ export default {
     },
   },
   methods: {
-    show() {
-      this.editing = this.startEdit
-      this.added = false
-      this.showModal = true
-      if (this.volunteering?.groups?.length > 0) {
-        this.groupid = this.volunteering.groups[0]
-      }
-    },
-    hide() {
-      this.editing = false
-      this.uploading = false
-      this.showModal = false
-    },
     validateTitle(value) {
       if (!value) {
         return 'Please enter a title.'
