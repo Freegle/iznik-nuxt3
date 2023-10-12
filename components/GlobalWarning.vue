9d2efa404a573f22b3f10008348aae23a9afb0c4
Edward Hibbert <edward@ehibbert.org.uk>
2023-10-12 15:53:42 +0100

    Add banner for Reuse Day.

--- components/GlobalWarning.vue
+++ components/GlobalWarning.vue
@@ -1,10 +1,31 @@
 <template>
-  <div v-if="false">
-    <NoticeMessage v-if="show" variant="danger">
+  <div v-if="true">
+    <NoticeMessage v-if="show" variant="info">
       <b-button variant="link" class="float-end" @click="hideit">
         Hide this
       </b-button>
-      <p>This would be a global alert.</p>
+      <div class="d-flex justify-content-between">
+        <b-img
+          src="/national_reuse_day_2023_Logo_round_500x500px.png"
+          style="width: 125px; height: 125px"
+          class="mr-2"
+        />
+        <div>
+          <p>
+            Are you ready to shine a light on reuse for
+            <b>#NationalReuseDay2023</b> on Friday 20 October? A day to
+            celebrate all things #Reuse, organised by Freegle, the reuse
+            experts.
+          </p>
+          <b-button
+            to="https://mailchi.mp/896a4360914b/freegle-bites-april-17353736"
+            variant="white"
+            class="mt-2"
+          >
+            Find out how to get involved
+          </b-button>
+        </div>
+      </div>
     </NoticeMessage>
     <div v-else class="text-danger text-end clickme" @click="showit">
       Show notice.
