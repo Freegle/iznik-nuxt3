47857dc23e4a4bbc6a61ceb5b2e90b3f3e33aa1a
Edward Hibbert <edward@ehibbert.org.uk>
2023-11-25 16:34:06 +0000

    One-click unsubscribe must be handled on the server, not the client.

--- pages/one-click-unsubscribe/[[uid]]/[[key]].vue
+++ pages/one-click-unsubscribe/[[uid]]/[[key]].vue
@@ -13,51 +13,49 @@
 import { useAuthStore } from '~/stores/auth'
 import { useRoute } from '#imports'
 
-onMounted(async () => {
-  const authStore = useAuthStore()
-  const route = useRoute()
+const authStore = useAuthStore()
+const route = useRoute()
 
-  const { key } = route.params
-  let { uid } = route.params
-  uid = parseInt(uid)
+const { key } = route.params
+let { uid } = route.params
+uid = parseInt(uid)
 
-  let showForgetFail = false
+let showForgetFail = false
 
-  if (uid && key) {
-    try {
-      // Log in using the username and key.
-      console.log('Login with', uid, key)
-      await authStore.login({
-        u: uid,
-        k: key,
-      })
+if (uid && key) {
+  try {
+    // Log in using the username and key.
+    console.log('Login with', uid, key)
+    await authStore.login({
+      u: uid,
+      k: key,
+    })
 
-      const loggedInAs = authStore.user?.id
-      console.log('Logged in as', loggedInAs)
+    const loggedInAs = authStore.user?.id
+    console.log('Logged in as', loggedInAs)
 
-      if (loggedInAs === uid) {
-        const ret = await authStore.forget()
-        console.log('Forget returned', ret)
+    if (loggedInAs === uid) {
+      const ret = await authStore.forget()
+      console.log('Forget returned', ret)
 
-        if (ret) {
-          showForgetFail = true
-        } else {
-          useRouter().push('/unsubscribe/unsubscribed')
-        }
+      if (ret) {
+        showForgetFail = true
+      } else {
+        useRouter().push('/unsubscribe/unsubscribed')
       }
-    } catch (e) {
-      // Login failed.  Usually this is because they're logged in as someone else. Ignore it.
-      console.log('Login failed', e)
-      showForgetFail = true
     }
-  } else {
+  } catch (e) {
+    // Login failed.  Usually this is because they're logged in as someone else. Ignore it.
+    console.log('Login failed', e)
     showForgetFail = true
   }
+} else {
+  showForgetFail = true
+}
 
-  if (showForgetFail) {
-    // Something went wrong with the one click unsubscribe.  Send them to the page that lets them try harder and
-    // contact support.
-    useRouter().push('/unsubscribe')
-  }
-})
+if (showForgetFail) {
+  // Something went wrong with the one click unsubscribe.  Send them to the page that lets them try harder and
+  // contact support.
+  useRouter().push('/unsubscribe')
+}
 </script>
