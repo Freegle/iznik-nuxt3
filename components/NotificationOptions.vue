53985531893364161cc15942b12a31e7bf1d452b
Edward Hibbert <edward@ehibbert.org.uk>
2023-12-08 13:28:16 +0000

    Mobile layout tweaks.

--- components/NotificationOptions.vue
+++ components/NotificationOptions.vue
@@ -5,7 +5,7 @@
     ref="theel"
     class="white text-center notification-list topstack"
     :variant="smallScreen ? 'transparent' : ''"
-    toggle-class="notification-list__dropdown-toggle p-md-0"
+    toggle-class="notification-list__dropdown-toggle p-xl-0"
     menu-class="notification-list__dropdown-menu"
     lazy
     right
