2f7b71545dbf180e360a89f3db3e55223b2021dc
Edward Hibbert <edward@ehibbert.org.uk>
2024-01-07 16:52:32 +0000

    Change chitchat mention tagger to remove weird Vue problems.

--- components/OurAtTa.vue
+++ components/OurAtTa.vue
@@ -1,14 +1,29 @@
 <template>
   <client-only>
-    <AtTa v-bind="$attrs">
-      <template v-for="(_, slot) of $slots" #[slot]="scope">
-        <slot :name="slot" v-bind="scope" />
-      </template>
-    </AtTa>
+    <Mentionable :keys="['@']" :items="items">
+      <slot name="default" />
+    </Mentionable>
   </client-only>
 </template>
 <script setup>
 // We use script setup because of https://github.com/nuxt/nuxt/issues/19964.  We put this into a subcomponent so that
 // we don't have to rework the other components which use it into script setup.
-import AtTa from 'vue-at/dist/vue-at-textarea'
+import 'floating-vue/dist/style.css'
+import { Mentionable } from 'vue-mention'
+
+const props = defineProps({
+  members: {
+    type: Array,
+    required: true,
+  },
+})
+
+const items = computed(() =>
+  props.members.map((m) => {
+    return {
+      value: m,
+      label: m,
+    }
+  })
+)
 </script>
