3adfc11d3f2300f2a3248893eca5346bd63d61e9
Edward Hibbert <edward@ehibbert.org.uk>
2024-01-18 17:25:45 +0000

    Switch to using Nuxt-native method for spotting when the site has been updated.

--- composables/useNavbar.js
+++ composables/useNavbar.js
@@ -8,7 +8,6 @@ import { useLogoStore } from '../stores/logo'
 import { useChatStore } from '../stores/chat'
 import { useAuthStore } from '~/stores/auth'
 import { fetchMe } from '~/composables/useMe'
-import { useRuntimeConfig } from '#app'
 
 export function useNavbar() {
   const authStore = useAuthStore()
@@ -52,8 +51,6 @@ export function useNavbar() {
   const showBackButton = computed(() => {
     // On mobile we want to show a back button instead of the logo when we're not on one of the "home" routes,
     // which are /browse, /chitchat, /myposts
-    console.log('Show back button', route?.path)
-
     return (
       route &&
       route.path !== '/browse' &&
