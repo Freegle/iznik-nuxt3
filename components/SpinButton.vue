e8233794bb881f1ec46d62ece427edc322cd9461
Edward Hibbert <edward@ehibbert.org.uk>
2024-01-24 14:58:04 +0000

    Sentry.

--- components/SpinButton.vue
+++ components/SpinButton.vue
@@ -128,10 +128,12 @@ const forgottenCallback = () => {
   if (useMiscStore().online) {
     finishSpinner()
     $sentryCaptureException(
-      'SpinButton - callback not called',
-      props.variant,
-      props.label,
-      props.iconName
+      'SpinButton - callback not called, ' +
+        props.variant +
+        ', ' +
+        props.label +
+        ', ' +
+        props.iconName
     )
   }
 }
