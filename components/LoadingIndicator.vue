ff7f1a7a2bbec5453f8d20ace58d301ba330ef67
Pavel Mironov <pashamironow@gmail.com>
2024-01-26 13:39:11 +0100

    add: loading indicator component

---
+++ components/LoadingIndicator.vue
@@ -0,0 +1,32 @@
+<template>
+  <div :style="{ opacity: isLoading ? 1 : 0 }">
+    <b-img
+      lazy
+      src="/loader.gif"
+      alt="Loading"
+      :width="`${width}px`"
+      :height="`${height}px`"
+    />
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  width: {
+    type: Number,
+    default: 100,
+  },
+  height: {
+    type: Number,
+    default: 100,
+  },
+  throttle: {
+    type: Number,
+    default: 0,
+  },
+})
+
+const { isLoading } = useLoadingIndicator({
+  throttle: props.throttle,
+})
+</script>
