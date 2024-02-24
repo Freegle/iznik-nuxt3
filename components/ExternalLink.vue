28fb619693b7ae4271687e4ecd39aa0ec2a2cf4d
Chris Cant <chris@phdcc.com>
2024-02-24 09:25:44 +0000

    Debug ExternalLink

--- components/ExternalLink.vue
+++ components/ExternalLink.vue
@@ -5,6 +5,7 @@
 <script>
 import { AppLauncher } from '@capacitor/app-launcher'
 import { useMobileStore } from '@/stores/mobile'
+import { useShortlinkStore } from '../stores/shortlinks'
 
 export default {
   props: {
@@ -13,11 +14,29 @@ export default {
       required: true,
     },
   },
+  async setup() {
+    const mobileStore = useMobileStore()
+    const shortlinkStore = useShortlinkStore()
+    return {
+      mobileStore,
+      shortlinkStore,
+    }
+  },
   methods: {
-    openInBrowser() {
-      const mobileStore = useMobileStore()
-      if (mobileStore.isApp) {
-        AppLauncher.openUrl({ url: this.href })
+    async openInBrowser() {
+      if (this.mobileStore.isApp) {
+        let url = this.href
+        /*const freeglein = 'https://freegle.in/'
+        if( url.startsWith(freeglein)){
+          const sname = url.substring(freeglein.length)
+          console.log('MATCH FREEGLE.IN', sname)
+          const sl = await this.shortlinkStore.fetch(26499)
+          console.log('sl', sl)
+          url = 'https://freegle.org.uk/businesscards/FreegleBusinessCardSmall.jpg' // https://ilovefreegle.org/businesscards/FreegleBusinessCardSmall.jpg
+          //const url = this.href.replace('ilovefreegle.org','freegle.org.uk')
+        }
+        console.log('openInBrowser',url)*/
+        AppLauncher.openUrl({ url })
         return false
       }
       return true
