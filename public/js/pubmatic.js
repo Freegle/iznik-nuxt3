10dce9a1eb4ca69a16be6f3da1c8ee46f48511e7
Edward Hibbert <edward@ehibbert.org.uk>
2024-02-27 11:07:04 +0000

    Pubmatic ad changes.

--- public/js/pubmatic.js
+++ public/js/pubmatic.js
@@ -49,27 +49,26 @@ setTimeout(loadGPT, 500);
 
 // ID hub code for EU traffic End
 
-// ID hub code for Non EU traffic start.  Commented out as for GDPR purposes we are inside the EU, even though we
-// aren't for political purposes.
+// ID hub code for Non EU traffic start.
 
-// (function() {
-//   var purl = window.location.href
-//   var url = '//ads.pubmatic.com/AdServer/js/pwt/164422/12427/'
-//   var profileVersionId = ''
-//   if (purl.indexOf('pwtv=') > 0) {
-//     var regexp = /pwtv=(.*?)(&|$)/g
-//     var matches = regexp.exec(purl)
-//     if (matches.length >= 2 && matches[1].length > 0) {
-//       profileVersionId = '/' + matches[1]
-//     }
-//   }
-//   var wtads = document.createElement('script')
-//   wtads.async = true
-//   wtads.type = 'text/javascript'
-//   wtads.src = url + profileVersionId + '/pwt.js'
-//   var node = document.getElementsByTagName('script')[0]
-//   node.parentNode.insertBefore(wtads, node)
-// })()
+(function() {
+  var purl = window.location.href
+  var url = '//ads.pubmatic.com/AdServer/js/pwt/164422/12427/'
+  var profileVersionId = ''
+  if (purl.indexOf('pwtv=') > 0) {
+    var regexp = /pwtv=(.*?)(&|$)/g
+    var matches = regexp.exec(purl)
+    if (matches.length >= 2 && matches[1].length > 0) {
+      profileVersionId = '/' + matches[1]
+    }
+  }
+  var wtads = document.createElement('script')
+  wtads.async = true
+  wtads.type = 'text/javascript'
+  wtads.src = url + profileVersionId + '/pwt.js'
+  var node = document.getElementsByTagName('script')[0]
+  node.parentNode.insertBefore(wtads, node)
+})()
 
 // ID hub code for Non EU traffic End
 
