84b3857de0db4be799c12aa325adbb873059b20b
Edward Hibbert <edward@ehibbert.org.uk>
2024-02-27 14:25:05 +0000

    Restore feed ad on mobile only.

--- components/MessageList.vue
+++ components/MessageList.vue
@@ -50,13 +50,15 @@
             />
           </VisibleWhen>
         </div>
-        <div v-if="insertAd(ix)" class="mt-3 mt-xl-2">
-          <ExternalDa
-            :ad-unit-path="insertAd(ix).adUnitPath"
-            :dimensions="insertAd(ix).dimensions"
-            :div-id="insertAd(ix).divId"
-          />
-        </div>
+        <VisibleWhen :at="['xs', 'sm', 'md']">
+          <div v-if="insertAd(ix)" class="mt-3 mt-xl-2">
+            <ExternalDa
+              :ad-unit-path="insertAd(ix).adUnitPath"
+              :dimensions="insertAd(ix).dimensions"
+              :div-id="insertAd(ix).divId"
+            />
+          </div>
+        </VisibleWhen>
       </div>
     </div>
     <infinite-loading
