10731f3426fee0a3172c94413e67dd611dc3ad10
Chris Cant <chris@phdcc.com>
2024-03-28 20:24:30 +0000

    Add pages/settings

---
+++ modtools/pages/settings.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="bg-white">
+    <b-tabs v-model="tabIndex" content-class="mt-3" card>
+      <b-tab>
+        <template v-slot:title>
+          <h2 class="ml-2 mr-2">
+            Personal
+          </h2>
+        </template>
+        <!--ModSettingsPersonal /-->
+      </b-tab>
+      <b-tab>
+        <template v-slot:title>
+          <h2 class="ml-2 mr-2">
+            Community
+          </h2>
+        </template>
+        <!--ModSettingsGroup :initial-group="loadGroup" /-->
+      </b-tab>
+      <b-tab>
+        <template v-slot:title>
+          <h2 class="ml-2 mr-2">
+            Standard Messages
+          </h2>
+        </template>
+        <!--ModSettingsModConfig /-->
+      </b-tab>
+      <b-tab>
+        <template v-slot:title>
+          <h2 class="ml-2 mr-2 text-muted">
+            Bulk Operations
+          </h2>
+        </template>
+        <p>
+          All the old bulk operations were to do with Yahoo. We will need to do some new ones for native groups.
+          TODO
+        </p>
+        <ul>
+          <li>Taking new members off moderation based on rules.</li>
+        </ul>
+      </b-tab>
+    </b-tabs>
+  </div>
+</template>
+
+<script setup>
+
+const tabIndex = ref(0)
+const loadGroup = ref(null)
+
+onMounted(() => {
+  /*if (this.loadGroup) {
+    // We've been asked to load group setting.
+    this.$nextTick(() => {
+      this.tabIndex = 1
+    })
+  }*/
+})
+
+</script>
