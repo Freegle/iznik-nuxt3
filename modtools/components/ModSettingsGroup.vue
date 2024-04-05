c62f96a4bb838fa1aa5bf597a944977d21b7a18f
Chris Cant <chris@phdcc.com>
2024-04-05 14:19:20 +0100

    Tidy

--- modtools/components/ModSettingsGroup.vue
+++ modtools/components/ModSettingsGroup.vue
@@ -3,9 +3,6 @@
     <div class="d-flex justify-content-between flex-wrap">
       <GroupSelect v-model="groupid" modonly />
     </div>
-    <div v-if="group" class="mt-2">
-      {{ group.namefull }}
-    </div>
     <div v-if="group && group.mysettings" class="mt-2">
       <NoticeMessage v-if="group.settings.closed" variant="danger" class="mb-1">
         Your community is currently closed. You can change this in <em>Features for Members</em>.
