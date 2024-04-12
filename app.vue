1d5df83dd2af5fc32333f27b44f0b7f7d128a928
Edward Hibbert <edward@ehibbert.org.uk>
2024-04-12 14:24:56 +0100

    Can't click Edit.

--- app.vue
+++ app.vue
@@ -5,7 +5,9 @@
         <NavbarDesktop />
         <NavbarMobile />
       </header>
-      <div class="position-absolute top-50 start-50 translate-middle z-3">
+      <div
+        class="position-absolute top-50 start-50 translate-middle z-3 pointer-none"
+      >
         <LoadingIndicator
           with-transition
           :throttle="loadingIndicatorThrottle"
@@ -259,5 +261,9 @@ ready = true
   background-color: transparent !important;
   color: transparent !important;
   border: none !important;
+}
+
+.pointer-none {
+  pointer-events: none;
 }
 </style>
