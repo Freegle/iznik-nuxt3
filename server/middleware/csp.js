2695e9cb11ba6f5544c09da914c7d63e23b1278f
Edward Hibbert <edward@ehibbert.org.uk>
2024-04-15 20:39:29 +0100

    Ads and CSP.

--- server/middleware/csp.js
+++ server/middleware/csp.js
@@ -1,7 +1,8 @@
 // CSP.  See also _hedaers.
 export default defineEventHandler((event) => {
-  event.node.res.setHeader(
-    'Content-Security-Policy',
-    "object-src 'none'; worker-src blob:; script-src 'self' 'unsafe-inline' 'unsafe-eval' *.facebook.net *.facebook.com matomo.ilovefreegle.org *.sentry.io *.google.com *.googletagservices.com *.gstatic.com accounts.google.com/gsi/client cdn.jsdelivr.net *.paypalobjects.com googleads.g.doubleclick.net *.gumgum.com *.adnxs.com securepubads.g.doubleclick.net *.googlesyndication.com *.netlify.app cdn-cookieyes.com api.rlcdn.com ads.pubmatic.com; report-uri https://fdapilive.ilovefreegle.org/csp.php"
-  )
+  // TODO Disabling while we experiment with ads.
+  // event.node.res.setHeader(
+  //   'Content-Security-Policy',
+  //   "object-src 'none'; worker-src blob:; script-src 'self' 'unsafe-inline' 'unsafe-eval' *.facebook.net *.facebook.com matomo.ilovefreegle.org *.sentry.io *.google.com *.googletagservices.com *.gstatic.com accounts.google.com/gsi/client cdn.jsdelivr.net *.paypalobjects.com googleads.g.doubleclick.net *.gumgum.com *.adnxs.com securepubads.g.doubleclick.net *.googlesyndication.com *.netlify.app cdn-cookieyes.com api.rlcdn.com ads.pubmatic.com; report-uri https://fdapilive.ilovefreegle.org/csp.php"
+  // )
 })
