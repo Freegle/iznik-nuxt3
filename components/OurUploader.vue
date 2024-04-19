fece9071efa5d60c8798db3c415e7f5d95ae5143
Edward Hibbert <edward@ehibbert.org.uk>
2024-04-19 14:37:29 +0100

    Update package.

--- components/OurUploader.vue
+++ components/OurUploader.vue
@@ -32,7 +32,6 @@
 </template>
 <script setup>
 // import * as LR from '@uploadcare/blocks'
-// TODO Waiting for official release.
 // TODO Rotate in microvolunteering and ModTools
 // TODO User edit of photo
 const LR = await import(
