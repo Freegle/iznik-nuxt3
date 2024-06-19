df1b7f7e3a0d7b44af81fc24787b27973a339a8a
Chris Cant <chris@phdcc.com>
2024-06-19 15:42:54 +0100

    Fix get all messages

--- modtools/composables/useModMessages.js
+++ modtools/composables/useModMessages.js
@@ -149,7 +149,7 @@ export function setupModMessages() {
       if (groupid.value) {
         messages = messageStore.getByGroup(groupid.value)
       } else {
-        messages = messageStore.getAll()
+        messages = messageStore.all()
       }
 
       show.value = messages.length
