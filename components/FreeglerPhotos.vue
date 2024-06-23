03b80a416ba5d0330298111e8757899e4560606f
Edward Hibbert <edward@ehibbert.org.uk>
2024-06-23 18:30:27 +0100

    More work on LCP.

--- components/FreeglerPhotos.vue
+++ components/FreeglerPhotos.vue
@@ -18,14 +18,14 @@
                   src="/landingpage/frame.png"
                   class-name="frame"
                   alt="Ornate gold picture frame. Image courtesy of https://pixabay.com/users/avantrend-321510/"
-                  sizes="0px sm:576px md:768"
+                  sizes="1px sm:576px md:768"
                 />
                 <ProxyImage
                   loading="lazy"
                   :src="photo(img)"
                   alt="Picture of a real freegler, looking happy. Photo by Alex Bamford."
                   class-name="image"
-                  sizes="0px sm:576px md:768"
+                  sizes="1px sm:576px md:768"
                 />
                 <p class="text-center text--smallest credit">
                   Photos of real freeglers, kindly taken by
@@ -44,14 +44,14 @@
               src="/landingpage/frame.png"
               class-name="frame"
               alt="Ornate gold picture frame. Image courtesy of https://pixabay.com/users/avantrend-321510/"
-              sizes="0px sm:576px md:768"
+              sizes="1px sm:576px md:768"
             />
             <ProxyImage
               preload
               :src="photo(1)"
               alt="Picture of a real freegler, looking happy. Photo by Alex Bamford."
               class-name="image"
-              sizes="0px sm:576px md:768"
+              sizes="1px sm:576px md:768"
             />
           </div>
         </template>
