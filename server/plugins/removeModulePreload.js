9c129a23dba033220f5caedaef4423a0247e9f30
Edward Hibbert <edward@ehibbert.org.uk>
2024-06-23 17:53:24 +0100

    Remove use of modulepreload.

--- server/plugins/removeModulePreload.js
+++ server/plugins/removeModulePreload.js
@@ -1,9 +1,11 @@
 export default defineNitroPlugin((nitroApp) => {
-  nitroApp.hooks.hook('render:html', (html, { event }) => {
-    // Remove from html.head any lines of <link rel="modulepreload" ...>
-    console.log('Remove modulepreload')
-    html.head = html.head.filter(
-      (line) => !line.startsWith('<link rel="modulepreload"')
+  nitroApp.hooks.hook('render:response', (response, { event }) => {
+    // Remove any modulepreload.  These are causing slow LCP because the client spends all its time
+    // loading and parsing Javascript it is unlikely ever to need rather than displaying the images
+    // and HTML it's rendered on the server.
+    response.body = response.body.replace(
+      /<link rel="modulepreload"[\s\S]*?>/g,
+      ''
     )
   })
 })
