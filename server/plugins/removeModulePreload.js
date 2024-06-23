3905776755068492e743b00446a3c9228cb2e304
Edward Hibbert <edward@ehibbert.org.uk>
2024-06-23 22:33:55 +0100

    Improve polyfills for LCP.

--- server/plugins/removeModulePreload.js
+++ server/plugins/removeModulePreload.js
@@ -3,9 +3,9 @@ export default defineNitroPlugin((nitroApp) => {
     // Remove any modulepreload.  These are causing slow LCP because the client spends all its time
     // loading and parsing Javascript it is unlikely ever to need rather than displaying the images
     // and HTML it's rendered on the server.
-    response.body = response.body.replace(
-      /<link rel="modulepreload"[\s\S]*?>/g,
-      ''
-    )
+    // response.body = response.body.replace(
+    //   /<link rel="modulepreload"[\s\S]*?>/g,
+    //   ''
+    // )
   })
 })
