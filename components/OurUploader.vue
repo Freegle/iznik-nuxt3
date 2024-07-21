74a57a6098cdfd78aa34e39c62153f2878b87638
Edward Hibbert <edward@ehibbert.org.uk>
2024-07-21 12:09:24 +0100

    Change camera facing mode.

--- components/OurUploader.vue
+++ components/OurUploader.vue
@@ -166,6 +166,7 @@ onMounted(() => {
     .use(Webcam, {
       modes: ['picture'],
       mobileNativeCamera: true,
+      facingMode: { exact: 'environment' },
     })
     .use(Tus, { endpoint: runtimeConfig.public.TUS_UPLOADER })
     .use(Compressor)
