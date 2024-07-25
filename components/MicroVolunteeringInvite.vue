32f517a5c1afc242c6eb7e883d5bfa1110763209
Edward Hibbert <edward@ehibbert.org.uk>
2024-07-25 11:38:05 +0100

    ContactPicker is rarely available.  Change how invitations work.

--- components/MicroVolunteeringInvite.vue
+++ components/MicroVolunteeringInvite.vue
@@ -9,7 +9,10 @@
       A personal recommendation is worth a thousand online ads - and anyway, we
       can't afford a thousand online ads!
     </p>
-    <p>Could you help spread the word?</p>
+    <p>
+      Could you help spread the word? Personalise your message and send it using
+      the buttons below.
+    </p>
     <InviteSomeone class="mt-2" :trust-pilot="false" @invited="invited" />
   </div>
 </template>
