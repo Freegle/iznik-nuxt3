e4993a162fb16ef523d868910b64d5ab2f0e9625
Edward Hibbert <edward@ehibbert.org.uk>
2024-07-30 08:57:01 +0100

    First pass National Reuse Day page.

---
+++ components/NationalReuseDayBox.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="box-wrapper">
+    <ExternalLink :href="url">
+      <div class="box" :class="boxClass">
+        <div class="button">
+          <h2 class="text-center">{{ title }}</h2>
+        </div>
+      </div>
+      <p class="text-center mt-2 font-weight-normal">
+        {{ description }}
+      </p>
+    </ExternalLink>
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+  title: String,
+  description: String,
+  url: String,
+  colour: String,
+})
+
+const boxClass = computed(() => {
+  const ret = {}
+  ret[props.colour] = true
+  return ret
+})
+</script>
+<style scoped lang="scss">
+@import 'bootstrap/scss/functions';
+@import 'bootstrap/scss/variables';
+@import 'bootstrap/scss/mixins/_breakpoints';
+
+.box {
+  border-radius: 10px;
+  border: 3px solid white;
+  padding: 10px;
+  width: 200px;
+  height: 150px;
+  align-content: center;
+}
+
+.box-wrapper {
+  width: 200px;
+}
+
+h2 {
+  color: white !important;
+}
+
+:deep(.magenta) {
+  background-color: #ff66c4;
+}
+
+.blue {
+  background-color: #38b6ff;
+}
+
+.purple {
+  background-color: #8c52ff;
+}
+
+.gold {
+  background-color: #ffd21d;
+}
+
+.orange {
+  background-color: #ffad4d;
+}
+
+.teal {
+  background-color: #10bbb0;
+}
+
+:deep(a) {
+  color: unset;
+  text-decoration: none;
+}
+</style>
---
+++ pages/NationalReuseDay.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="wrapper d-flex flex-column justify-content-around">
+    <div class="inner">
+      <h1 class="text-center text-white">National Reuse Day 2024</h1>
+      <p class="text-center">Intro text blah blah blah</p>
+      <div class="d-flex justify-content-around">
+        <ProxyImage
+          preload
+          src="/councils/NationalReuseDay.png"
+          alt="National Reuse Day logo"
+          class-name="image"
+        />
+      </div>
+      <div class="spacer" />
+      <div class="boxes">
+        <NationalReuseDayBox
+          title="Video"
+          description="Watch and share our new A-Z of Freegle!"
+          url="https://example.com"
+          colour="magenta"
+        />
+        <NationalReuseDayBox
+          title="Resources"
+          description="Free communications assets and key links"
+          url="https://councils.ilovefreegle.org/"
+          colour="blue"
+        />
+        <NationalReuseDayBox
+          title="Share your reuse story"
+          description="Tell us why you love Freegle and find out what others are saying too"
+          url="https://www.ilovefreegle.org/stories"
+          colour="purple"
+        />
+        <NationalReuseDayBox
+          title="Freegle something"
+          description="Give and get for free, locally!"
+          url="https://www.ilovefreegle.org"
+          colour="gold"
+        />
+        <NationalReuseDayBox
+          title="Quiz"
+          description="Test your knowledge and discover fun facts about reuse!"
+          url="https://councils.ilovefreegle.org/"
+          colour="orange"
+        />
+        <NationalReuseDayBox
+          title="Refer a friend to Freegle"
+          description="Get more reuse happening by spreading the word"
+          url="https://www.ilovefreegle.org/promote"
+          colour="blue"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+definePageMeta({
+  layout: 'no-navbar',
+})
+
+onMounted(() => {
+  // Remove nav elements from the DOM.
+  document.querySelector('.navbar').remove()
+})
+</script>
+<style scoped lang="scss">
+@import 'bootstrap/scss/functions';
+@import 'bootstrap/scss/variables';
+@import 'bootstrap/scss/mixins/_breakpoints';
+
+.wrapper {
+  background-color: #5eca24;
+  color: $color-white;
+  font-weight: bold;
+  height: 100vh;
+}
+
+.spacer {
+  height: 100px;
+}
+
+.boxes {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr;
+  grid-column-gap: 50px;
+  grid-row-gap: 50px;
+  justify-items: center;
+
+  @include media-breakpoint-up(md) {
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr;
+  }
+}
+
+:deep(.image) {
+  width: 50vw;
+}
+</style>
