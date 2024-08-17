5f0cd56bdc48e17718385958290813a14865c16b
Edward Hibbert <edward@ehibbert.org.uk>
2024-08-17 11:52:48 +0100

    Setting wrong env variable.

--- setvars.cmd
+++ setvars.cmd
@@ -4,4 +4,4 @@ set IZNIK_API_V2=http://127.0.0.1:8192/api
 set TRUSTPILOT_LINK=https://uk.trustpilot.com/evaluate/ilovefreegle.org
 set NODE_OPTIONS=--max_old_space_size=6000
 set GOOGLE_ADSENSE_TEST_MODE=false
-set GOOGLE_ADSENSE_TEST_MODE=ca-pub-9017028318226154
+set GOOGLE_ADSENSE_ID=ca-pub-9017028318226154
