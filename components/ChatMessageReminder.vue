102d92e71e95f3a687aa20b39adb12eff0d33673
Edward Hibbert <edward@ehibbert.org.uk>
2024-08-20 16:06:17 +0100

    Introduce Reminder type into chat for collection reminders.

--- components/ChatMessageReminder.vue
+++ components/ChatMessageReminder.vue
@@ -30,59 +30,13 @@
   </div>
 </template>
 <script>
-import { useComposeStore } from '../stores/compose'
-import { fetchReferencedMessage } from '../composables/useChat'
 import ChatBase from '~/components/ChatBase'
-import { useRouter } from '#imports'
 
 export default {
   extends: ChatBase,
-  async setup(props) {
-    const composeStore = useComposeStore()
-
-    await fetchReferencedMessage(props.chatid, props.id)
-
-    return { composeStore }
-  },
-  data: function () {
-    return {
-      contactGroupId: null,
-    }
-  },
   computed: {
-    group() {
-      return this.chat && this.chat.group ? this.chat.group : null
-    },
     amUser() {
       return this.chat && this.chat.user && this.chat.user.id === this.myid
-    },
-  },
-  methods: {
-    async repost() {
-      const message = Object.assign({}, this.refmsg)
-
-      if (message) {
-        // Remove any partially composed messages we currently have, because they'll be confusing.
-        await this.composeStore.clearMessages()
-
-        // Add this message to the compose store so that it will show up on the compose page.
-        await this.composeStore.setMessage(
-          0,
-          {
-            type: message.type,
-            item: message.item?.name?.trim(),
-            description: message.textbody.trim(),
-            availablenow: message.availablenow,
-            repostof: this.chatmessage.refmsgid,
-          },
-          this.me
-        )
-
-        this.composeStore.setAttachmentsForMessage(0, message.attachments)
-
-        const router = useRouter()
-        router.push(message.type === 'Offer' ? '/give' : '/find')
-      }
     },
   },
 }
