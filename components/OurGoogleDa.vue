f5c3162904b2d12e4e33c69ef6cfaa03001010ce
Edward Hibbert <edward@ehibbert.org.uk>
2024-08-24 10:28:49 +0100

    Try "donate and we'll turn ads off for a month"

--- components/OurGoogleDa.vue
+++ components/OurGoogleDa.vue
@@ -1,8 +1,8 @@
 <template>
   <div v-if="showAd">
     <div v-if="adsBlocked" class="bg-white text-center">
-      Maybe you're using an ad blocker? We don't like ads much either. If we
-      raised enough in donations, we could turn them off for everyone.
+      Maybe you're using an ad blocker? We don't like ads much either. If you
+      donate, we'll turn ads for you for a month.
       <donation-button value="1" class="mb-1" text="Donate £1" />
     </div>
     <div
