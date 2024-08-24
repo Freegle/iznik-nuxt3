42fe678c06be2b67db3105e2cf4f5acecfa6abe6
Edward Hibbert <edward@ehibbert.org.uk>
2024-08-24 10:37:00 +0100

    Tweak adsoff wording.

--- pages/adsoff.vue
+++ pages/adsoff.vue
@@ -4,7 +4,7 @@
       <b-col cols="0" md="3" />
       <b-col cols="12" md="6" class="mt-2">
         <b-card no-body class="mt-1 p-4">
-          <h1>Free To Use - Not Free To Run!</h1>
+          <h1>Love Freegle, Hate Ads?</h1>
           <div class="d-flex">
             <div>
               <p>
@@ -13,23 +13,22 @@
                 >
               </p>
               <p>
-                There are some things we have to pay for to keep going. Ads
-                support this - but wouldn't it be nice not to have them?
+                Our charity is free to use but not free to run. There are some
+                things we have to pay for to keep going. Ads support this - but
+                wouldn't it be nice not to have them?
               </p>
-              <p v-if="adsOffTarget <= 0">
-                Right now we've raised enough in donations over the last 24
-                hours, so ads are off for everyone. Thanks!
+              <p>
+                <strong
+                  >If you donate, we'll turn ads off for you for a
+                  month:</strong
+                >
               </p>
-              <p v-else>
-                <span v-if="recentDonor"
-                  >You've donated, so ads are off for you.
-                </span>
-                Right now we still need &pound;{{ adsOffTarget }}
-
-                to reach our donation target of &pound;{{ adsOffTargetMax }}
-                over the last 24 hours. Once we reach that, we'll turn ads off
-                for everyone.
-              </p>
+              <div class="d-flex flex-wrap justify-content-between mt-2 mb-3">
+                <donation-button value="1" class="mb-1" @clicked="score(1)" />
+                <donation-button value="5" class="mb-1" @clicked="score(5)" />
+                <donation-button value="10" class="mb-1" @clicked="score(10)" />
+              </div>
+              <p>Regular monthly donations are especially helpful.</p>
             </div>
             <vue-thermometer
               :key="adsOffTarget"
@@ -40,17 +39,19 @@
               scale="£"
             />
           </div>
-          <p>
-            <strong
-              >If you donate, we'll turn ads off for you for a month:</strong
-            >
+          <p v-if="adsOffTarget <= 0">
+            Right now we've raised enough in donations over the last 24 hours,
+            so ads are off for everyone. Thanks!
           </p>
-          <div class="d-flex flex-wrap justify-content-between mt-2 mb-3">
-            <donation-button value="1" class="mb-1" @clicked="score(1)" />
-            <donation-button value="5" class="mb-1" @clicked="score(5)" />
-            <donation-button value="10" class="mb-1" @clicked="score(10)" />
-          </div>
-          <p>Regular monthly donations are especially helpful.</p>
+          <p v-else>
+            <span v-if="recentDonor"
+              >You've donated, so ads are off for you.
+            </span>
+            Right now we still need &pound;{{ adsOffTarget }} to reach our
+            donation target of &pound;{{ adsOffTargetMax }}
+            over the last 24 hours. Once we reach that, we'll turn ads off for
+            everyone.
+          </p>
           <p>
             Other ways to donate are <nuxt-link to="/donate">here</nuxt-link>.
           </p>
