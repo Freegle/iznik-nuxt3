3324ebe90e57e09c7b124297a1dccfb4299f5b1f
Edward Hibbert <edward@ehibbert.org.uk>
2024-09-14 17:30:21 +0100

    Update Oxford High logo

--- app.vue
+++ app.vue
@@ -32,7 +32,7 @@
                   alt="Freegle Logo"
                   title="Click to go to Home page"
                   class="logo mr-2 rounded"
-                  src="/icon.png?oxfordhigh3"
+                  src="/icon.png%3Foxfordhigh4"
                   sizes="58px"
                 />
               </a>
