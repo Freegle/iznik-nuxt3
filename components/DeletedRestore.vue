f7970b9e8f2d9d9acd81e4caf45e34b30ec73254
Edward Hibbert <edward@ehibbert.org.uk>
2024-10-29 08:25:16 +0000

    Handle case where someone has left but there is still an outstanding session or login link.

--- components/DeletedRestore.vue
+++ components/DeletedRestore.vue
@@ -1,29 +1,50 @@
 <template>
   <b-row>
-    <b-col cols="12" xl="6" offset-xl="3" class="bottom verytop">
+    <b-col
+      cols="12"
+      xl="6"
+      offset-xl="3"
+      class="verytop"
+      :class="{
+        bottom,
+      }"
+    >
       <NoticeMessage
-        v-if="me && me.deleted"
+        v-if="me?.deleted"
         variant="danger"
         class="mb-3 text-center"
       >
-        <p class="text--large font-weight-bold">
-          <v-icon icon="exclamation-triangle" />
-          You deleted your account {{ timeago(me.deleted) }}. It will be
-          completely removed soon.
-        </p>
-        <p class="font-weight-bold">
-          Meanwhile, other freeglers can't see your details, posts or chats.
-        </p>
-        <p>
-          If you've done this by mistake, or changed your mind, please click
-          here:
-        </p>
-        <div class="d-flex justify-content-around">
-          <SpinButton
-            label="Restore your account"
-            variant="primary"
-            @handle="restore"
-          />
+        <div v-if="me.forgotten">
+          <p class="text--large font-weight-bold">
+            <v-icon icon="exclamation-triangle" />
+            You deleted your account {{ timeago(me.deleted) }}. Your data has
+            now been removed.
+          </p>
+          <p>If you'd like to rejoin, we'd love to have you.</p>
+          <b-button variant="primary" class="mt-2" size="lg" @click="rejoin"
+            >Rejoin Freegle</b-button
+          >
+        </div>
+        <div v-else>
+          <p class="text--large font-weight-bold">
+            <v-icon icon="exclamation-triangle" />
+            You deleted your account {{ timeago(me.deleted) }}. It will be
+            completely removed soon.
+          </p>
+          <p class="font-weight-bold">
+            Meanwhile, other freeglers can't see your details, posts or chats.
+          </p>
+          <p>
+            If you've done this by mistake, or changed your mind, please click
+            here:
+          </p>
+          <div class="d-flex justify-content-around">
+            <SpinButton
+              label="Restore your account"
+              variant="primary"
+              @handle="restore"
+            />
+          </div>
         </div>
       </NoticeMessage>
     </b-col>
@@ -34,9 +55,25 @@ import { useAuthStore } from '../stores/auth'
 
 const authStore = useAuthStore()
 
+defineProps({
+  bottom: {
+    type: Boolean,
+    required: false,
+    default: true,
+  },
+})
+
 async function restore(handle) {
   await authStore.restore()
   handle()
+}
+
+async function rejoin() {
+  await authStore.logout()
+
+  // Go to /
+  const router = useRouter()
+  router.push('/')
 }
 </script>
 <style scoped>
