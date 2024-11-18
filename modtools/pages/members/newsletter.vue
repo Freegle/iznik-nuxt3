75bee151837b3d99bb4af202036214e1635f2f4d
Chris Cant <chris@phdcc.com>
2024-11-18 10:29:13 +0000

    Add stub for Members+Newsletter

---
+++ modtools/pages/members/newsletter.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <ScrollToTop />
+    <NoticeMessage v-if="!stories || !stories.length" variant="info" class="mt-2">
+      There are no stories to review at the moment.
+    </NoticeMessage>
+    <div v-for="story in stories" :key="'story-' + story.id" class="mt-2">
+      <ModStoryReview :story="story" newsletter />
+    </div>
+  </div>
+</template>
+<script>
+
+export default {
+  components: {
+  },
+  computed: {
+    stories() {
+      /*const stories = this.$store.getters['stories/list']
+
+      if (stories) {
+        return Object.values(stories)
+      }*/
+
+      return []
+    }
+  },
+  async fetch() {
+    /*await this.$store.dispatch('stories/fetch', {
+      newsletter: true
+    })*/
+  },
+
+  methods: {}
+}
+</script>
