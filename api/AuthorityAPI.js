83ee25ea432c150acabc35fb296673eb9d1c796a
Edward Hibbert <edward@ehibbert.org.uk>
2024-12-07 19:56:46 +0000

    First draft Essex landing page

--- api/AuthorityAPI.js
+++ api/AuthorityAPI.js
@@ -4,4 +4,8 @@ export default class AuthorityAPI extends BaseAPI {
   fetch(params) {
     return this.$get('/authority', params)
   }
+
+  async fetchMessages(id) {
+    return await this.$getv2('/authority/' + id + '/message')
+  }
 }
