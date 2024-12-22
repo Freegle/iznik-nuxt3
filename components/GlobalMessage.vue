9e33454b2037b8a31d190cdbf04f67bf6587d7d9
Edward Hibbert <edward@ehibbert.org.uk>
2024-12-22 11:31:55 +0000

    Add Christmas Pledge

--- components/GlobalMessage.vue
+++ components/GlobalMessage.vue
@@ -1,34 +1,36 @@
 <template>
-  <div v-if="!me?.settings?.pledge2025">
+  <div v-if="me?.settings?.pledge2025">
     <PrivacyUpdate />
     <div v-if="new Date().getTime() < new Date('2025-01-03')">
-      <b-card v-if="show" body-class="d-flex justify-content-around">
-        <b-img
-          src="/logos/Christmas.gif"
-          alt="Christmas GIF"
-          class="christmas rounded"
-        />
-        <div class="ml-2 font-weight-bold">
-          <div v-if="thanks">
-            <p>
+      <b-card v-if="show">
+        <div class="christmaslayout">
+          <b-img
+            src="/logos/Christmas.gif"
+            alt="Christmas GIF"
+            class="christmas rounded logo"
+          />
+          <template v-if="thanks">
+            <p class="thanks">
               Thanks! You'll be making the world better, one act of kindness at
               a time.
             </p>
-          </div>
-          <div v-else>
-            <p>
+          </template>
+          <template v-else>
+            <p class="merry">
               Merry Whatevermas to you and yours, from all the volunteers at
               Freegle!
             </p>
-            <p>
+            <p class="resolution">
               And if you're making resolutions this New Year, why not pledge to
               freegle one extra thing a month throughout the year? We'll track
               your freegles and let you know how you do!
             </p>
-            <b-button size="lg" variant="primary" @click="pledge">
-              I'll sign the Freegle pledge!
-            </b-button>
-          </div>
+            <div class="pledge">
+              <b-button size="lg" variant="primary" @click="pledge">
+                I'll sign the Freegle pledge!
+              </b-button>
+            </div>
+          </template>
         </div>
       </b-card>
       <div v-else class="text-danger text-end clickme" @click="showit">
@@ -127,9 +129,55 @@ export default {
 
 :deep(.christmas) {
   max-width: 100px;
+  max-height: 100px;
 
   @include media-breakpoint-up(md) {
     max-width: 200px;
+    max-height: 200px;
+  }
+}
+
+.christmaslayout {
+  display: grid;
+  grid-template-rows: min-content min-content min-content;
+  grid-template-columns: min-content 1fr;
+  grid-column-gap: 1rem;
+  font-weight: bold;
+
+  .logo,
+  .thanks {
+    grid-row: 1 / 2;
+    grid-column: 1 / 2;
+
+    @include media-breakpoint-up(md) {
+      grid-row: 1 / 3;
+      grid-column: 1 / 2;
+    }
+  }
+
+  .merry {
+    grid-row: 1 / 2;
+    grid-column: 2 / 3;
+  }
+
+  .resolution {
+    grid-row: 2 / 3;
+    grid-column: 1 / 3;
+
+    @include media-breakpoint-up(md) {
+      grid-row: 2 / 3;
+      grid-column: 2 / 3;
+    }
+  }
+
+  .pledge {
+    grid-row: 3 / 4;
+    grid-column: 1 / 3;
+
+    @include media-breakpoint-up(md) {
+      grid-row: 3 / 4;
+      grid-column: 2 / 3;
+    }
   }
 }
 </style>
