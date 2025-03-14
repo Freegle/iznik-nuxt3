ef1e09ef809a466e49d0ba257c54b77b99f024c7
Chris Cant <chris@phdcc.com>
2025-03-14 13:44:33 +0000

    Debug yahoo login

--- modtools/middleware/authuser.global.ts
+++ modtools/middleware/authuser.global.ts
@@ -7,12 +7,13 @@ import { useAuthStore } from '~/stores/auth'
 export default defineNuxtRouteMiddleware((to, from) => {
   console.log('authuser.global.ts',to.path)
   const authStore = useAuthStore()
-  if (to.path === '/login' || to.path === '/forgot' || to.path === '/yahoologin') {
+  if (to.path === '/login' || to.path === '/forgot' || to.path === '/yahoologin/') {
     // Accessing login, forgot and yahoologin pages always OK
     return
   }
   const me = authStore.user
   if (me === null || !me.id) {
+    console.log('authuser.global.ts PLEASE LOGIN')
     // If me not set yet, then go to login page and bounce back if need be
     return navigateTo('/login?return='+to.path)
   }
