a9e4d00a16c7f640c7efbde531246a85e6867c52
Edward Hibbert <edward@ehibbert.org.uk>
2025-03-14 17:10:24 +0000

    If can find a postcode in a normal text chat message, show a minimap.

--- components/ChatMessageText.vue
+++ components/ChatMessageText.vue
@@ -4,44 +4,57 @@
     :class="{ myChatMessage: messageIsFromCurrentUser }"
   >
     <div class="chatMessage forcebreak chatMessage__owner">
-      <span v-if="!highlightEmails">
-        <span v-if="messageIsNew" class="prewrap font-weight-bold">{{
-          emessage
-        }}</span>
-        <span v-else class="preline forcebreak">{{ emessage }}</span>
-        <b-img
-          v-if="chatmessage?.image"
-          fluid
-          :src="chatmessage?.image.path"
-          lazy
-          rounded
-        />
-      </span>
-      <span v-else>
-        <span v-if="messageIsNew" class="font-weight-bold">
-          <Highlighter
-            :text-to-highlight="emessage"
-            :search-words="[regexEmail]"
-            highlight-class-name="highlight"
-            class="prewrap"
+      <div>
+        <span v-if="!highlightEmails">
+          <span v-if="messageIsNew" class="prewrap font-weight-bold">{{
+            emessage
+          }}</span>
+          <span v-else class="preline forcebreak">{{ emessage }}</span>
+          <b-img
+            v-if="chatmessage?.image"
+            fluid
+            :src="chatmessage?.image.path"
+            lazy
+            rounded
           />
         </span>
         <span v-else>
-          <Highlighter
-            :text-to-highlight="emessage"
-            :search-words="[regexEmail]"
-            highlight-class-name="highlight"
-            class="preline forcebreak"
+          <span v-if="messageIsNew" class="font-weight-bold">
+            <Highlighter
+              :text-to-highlight="emessage"
+              :search-words="[regexEmail]"
+              highlight-class-name="highlight"
+              class="prewrap"
+            />
+          </span>
+          <span v-else>
+            <Highlighter
+              :text-to-highlight="emessage"
+              :search-words="[regexEmail]"
+              highlight-class-name="highlight"
+              class="preline forcebreak"
+            />
+          </span>
+          <b-img
+            v-if="chatmessage?.image"
+            fluid
+            :src="chatmessage?.image.path"
+            lazy
+            rounded
           />
         </span>
-        <b-img
-          v-if="chatmessage?.image"
-          fluid
-          :src="chatmessage?.image.path"
-          lazy
-          rounded
-        />
-      </span>
+      </div>
+      <l-map
+        v-if="lat || lng"
+        ref="map"
+        :zoom="16"
+        :max-zoom="maxZoom"
+        :center="[lat, lng]"
+        :style="'width: 100%; height: 200px'"
+      >
+        <l-tile-layer :url="osmtile" :attribution="attribution" />
+        <l-marker :lat-lng="[lat, lng]" :interactive="false" />
+      </l-map>
     </div>
     <div class="chatMessageProfilePic">
       <ProfileImage
@@ -57,6 +70,9 @@
 import Highlighter from 'vue-highlight-words'
 import ChatBase from '~/components/ChatBase'
 import ProfileImage from '~/components/ProfileImage'
+import { MAX_MAP_ZOOM, POSTCODE_REGEX } from '~/constants'
+import { attribution, osmtile } from '~/composables/useMap'
+import { useLocationStore } from '~/stores/location'
 
 export default {
   components: {
@@ -64,13 +80,54 @@ export default {
     Highlighter,
   },
   extends: ChatBase,
+  data: function () {
+    return {
+      lat: null,
+      lng: null,
+    }
+  },
   computed: {
+    osmtile: () => osmtile(),
+    attribution: () => attribution(),
+    maxZoom() {
+      return MAX_MAP_ZOOM
+    },
     messageIsNew() {
       return (
         this.chatmessage?.secondsago < 60 ||
         this.chatmessage?.id > this.chat?.lastmsgseen
       )
     },
+    postcode() {
+      let ret = null
+
+      const postcode = this.chatmessage?.message.match(POSTCODE_REGEX)
+
+      if (postcode?.length) {
+        if (!postcode[0].includes(' ')) {
+          // Make sure we have a space in the right place, because this helps with autocomplete
+          ret = postcode[0].replace(/^(.*)(\d)/, '$1 $2')
+        } else {
+          ret = postcode[0]
+        }
+      }
+
+      return ret
+    },
+  },
+  async mounted() {
+    console.log('Mounted, postcode', this.postcode)
+    if (this.postcode) {
+      // Use typeahead to find the postcode location.
+      const locationStore = useLocationStore()
+      const locs = await locationStore.typeahead(this.postcode)
+      console.log('Typeahead returned', locs)
+
+      if (locs?.length) {
+        this.lat = locs[0].lat
+        this.lng = locs[0].lng
+      }
+    }
   },
 }
 </script>
