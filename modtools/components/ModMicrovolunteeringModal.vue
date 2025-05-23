3dcf3e2c479de51134b9b16e3f6db1e75fb90284
Chris Cant <chris@phdcc.com>
2025-05-23 14:21:15 +0100

    Add modtools support layer

---
+++ modtools/components/ModMicrovolunteeringModal.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <client-only>
+      <b-modal ref="modal" id="aboutmemodal" :title="'Microvolunteering for ' + user.displayname" size="xl">
+        <template #default>
+          <div v-for="item in itemIds" :key="'item-' + item" class="p-0 mt-2">
+            <ModMicrovolunteering :id="item" />
+          </div>
+        </template>
+        <template #footer>
+          <b-button variant="white" @click="hide">
+            Cancel
+          </b-button>
+        </template>
+      </b-modal>
+    </client-only>
+  </div>
+</template>
+<script>
+import { useOurModal } from '~/composables/useOurModal'
+
+export default {
+  setup() {
+    const { modal, hide } = useOurModal()
+    return { modal, hide }
+  },
+  props: {
+    user: {
+      type: Object,
+      required: true
+    },
+    itemIds: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    show() {
+      this.modal.show()
+    }
+  }
+}
+</script>
