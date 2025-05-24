8a91896bb3c265cf265b8b1b9526c802587813b6
Edward Hibbert <edward@ehibbert.org.uk>
2025-05-24 12:30:30 +0100

    WIP Playwright tests.

--- components/NavbarDesktop.vue
+++ components/NavbarDesktop.vue
@@ -297,10 +297,12 @@ const {
   maybeReload,
 } = useNavbar()
 
+console.log('NavbarDesktop mounted, start with signInDisabled')
 const signInDisabled = ref(true)
 
 onMounted(() => {
   // Keeping the button disabled until hygration has finished helps with Playwright tests.
+  console.log('NavbarDesktop mounted, setting signInDisabled to false')
   signInDisabled.value = false
 })
 
