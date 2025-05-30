d43c5ebb9c487694445e433faa64c12c75ca4b5c
Chris Cant <chris@phdcc.com>
2025-05-30 09:34:46 +0100

    Add discourse route to allow logging in there

---
+++ modtools/pages/discourse.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="bg-white">
+    <p>This should redirect you back to Discourse. If it doesn't, mail geeks@ilovefreegle.org.</p>
+    <b-img src="~/static/loader.gif" alt="Loading" />
+  </div>
+</template>
+<script>
+export default {
+  watch: {
+    myid(newVal, oldVal) {
+      console.log('modtools discourse watch myid', newVal, oldVal)
+      if (!oldVal && newVal) {
+        this.redirect()
+      }
+    }
+  },
+  mounted() {
+    console.log('modtools discourse mounted', this.myid)
+    if (this.myid) {
+      this.redirect()
+    }
+  },
+  methods: {
+    redirect() {
+      console.log('modtools discourse redirect')
+      window.location = 'https://discourse.ilovefreegle.org'
+    }
+  }
+}
+</script>
