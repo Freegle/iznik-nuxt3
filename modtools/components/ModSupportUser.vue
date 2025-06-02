e11df37e022adcb2d21ed1ecceaf9e663c3de24d
Chris Cant <chris@phdcc.com>
2025-06-02 10:37:13 +0100

    Fix Support find user

--- modtools/components/ModSupportUser.vue
+++ modtools/components/ModSupportUser.vue
@@ -552,10 +552,10 @@ export default {
   methods: {
     async fetchUser() {
       if (this.id) {
-        await this.userStore.fetchMT({ search: this.id, emailhistory: true, info: true })
+        await this.userStore.fetchMT({ id: this.id, emailhistory: true, info: true })
         this.user = this.userStore.byId(this.id)
         if (this.user && this.user.spammer && this.user.spammer.byuserid) {
-          await this.userStore.fetchMT({ search: this.user.spammer.byuserid })
+          await this.userStore.fetchMT({ id: this.user.spammer.byuserid })
           this.user.spammer.byuser = await this.userStore.fetch(this.user.spammer.byuserid)
         }
       }
@@ -624,7 +624,7 @@ export default {
       console.log('updateComments', userid)
 
       await this.userStore.fetchMT({
-        search: userid,
+        id: userid,
         emailhistory: true
       })
       await this.fetchUser()
