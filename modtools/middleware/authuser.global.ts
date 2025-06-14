c29d28348989c814f1bc2d6a8bb4ea2264e30dcb
Chris Cant <chris@phdcc.com>
2025-06-14 20:49:42 +0100

    MT: Make impersonate work.

--- modtools/middleware/authuser.global.ts
+++ modtools/middleware/authuser.global.ts
@@ -7,14 +7,35 @@ import { useAuthStore } from '~/stores/auth'
 export default defineNuxtRouteMiddleware((to, from) => {
   // console.log('authuser.global.ts',to.path)
   const authStore = useAuthStore()
-  //if (to.path === '/login' || to.path === '/forgot' || to.path === '/yahoologin/') {
-  if (to.path === '/login' || to.path === '/forgot' || to.path === '/yahoologin/' || to.path === '/discourse') {
+  // if (to.path === '/login' || to.path === '/forgot' || to.path === '/yahoologin/') {
+  if (
+    to.path === '/login' ||
+    to.path === '/forgot' ||
+    to.path === '/yahoologin/' ||
+    to.path === '/discourse'
+  ) {
     // Accessing login, forgot, yahoologin and discourse pages always OK
     return
   }
   const me = authStore.user
   if (me === null || !me.id) {
     // If me not set yet, then go to login page and bounce back if need be
-    return navigateTo('/login?return='+to.path)
+    let newTo = '/login?return=' + to.path
+    const newParams = []
+
+    console.log('To', to)
+    for (const key in to.query) {
+      if (Object.prototype.hasOwnProperty.call(to.query, key)) {
+        newParams.push(key + '=' + encodeURIComponent(to.query[key]))
+      }
+    }
+
+    console.log('newParams', newParams)
+
+    if (newParams.length) {
+      newTo += '&' + newParams.join('&')
+    }
+
+    return navigateTo(newTo)
   }
 })
