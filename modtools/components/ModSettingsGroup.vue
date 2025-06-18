8bf4b5d8b0a63c07914480db806df19e290674d3
edwh <edward@ehibbert.org.uk>
2025-06-18 08:25:11 +0100

    Lint.

--- modtools/components/ModSettingsGroup.vue
+++ modtools/components/ModSettingsGroup.vue
@@ -1073,8 +1073,9 @@
               <!-- eslint-disable-next-line -->
               <ExternalLink href="https://arthur-e.github.io/Wicket/sandbox-gmaps3.html">this tool</ExternalLink>.
               >. >. >. >. >. >. >. >. >. >. >. >. >. >. >. >. >. >. >. >. >. >.
-              >. >. >. >. >. >. >. >. >. >. >. >. >. >. >. >. >. >. You can also
-              view all community areas
+              >. >. >. >. >. >. >. >. >. >. >. >. >. >. >. >. >. >. >. >. >. >.
+              >. >. >. >. >. >. >. >. >. >. >. >. >. >. >. >. You can also view
+              all community areas
               <!-- eslint-disable-next-line -->
               <nuxt-link to="/map">here</nuxt-link>,
               or all caretaker communities
