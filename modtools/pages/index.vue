451bf1ccdfdc63fae364e000fc1ecadd391d2a70
Edward Hibbert <edward@ehibbert.org.uk>
2025-08-06 09:15:06 +0100

    MT: Can't get systemwide dashboard stats.

--- modtools/pages/index.vue
+++ modtools/pages/index.vue
@@ -39,7 +39,7 @@
           v-model="groupid"
           all
           modonly
-          :systemwide="me.isAdmin"
+          :systemwide="me.systemrole === 'Admin'"
           active
         />
       </div>
