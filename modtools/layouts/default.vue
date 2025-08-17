fd44113ae75547538da1c00f6856293400c92988
Chris Cant <chris@phdcc.com>
2025-08-17 09:40:49 +0100

    Get inMTapp from routechanged as well

--- modtools/layouts/default.vue
+++ modtools/layouts/default.vue
@@ -344,7 +344,7 @@ useHead({
 
 const loginStateKnown = computed(() => authStore.loginStateKnown)
 const loggedIn = computed(() => authStore.user !== null)
-const inMTapp = computed(() => window.sessionStorage?.getItem('inMTapp'))
+const inMTapp = ref(false)
 
 const discourseCount = computed(() => {
   const discourse = authStore.discourse
@@ -382,6 +382,13 @@ watch(
     }
 
     if (routechanged) {
+      if (route.query?.inMTapp) {
+        const queryinMTapp =
+          route.query.inMTapp === 'true' ? '0.4.6' : route.query.inMTapp
+        console.error('queryinMTapp', queryinMTapp)
+        window.sessionStorage.setItem('inMTapp', queryinMTapp)
+        inMTapp.value = queryinMTapp
+      }
       // Get per-group-work and ensure all current groups are in modGroupStore
       await modGroupStore.getModGroups()
     }
@@ -401,6 +408,8 @@ watch(
 
 // Lifecycle hooks and watches
 onMounted(async () => {
+  inMTapp.value = window.sessionStorage?.getItem('inMTapp')
+
   // For this layout we don't need to be logged in.  So can just continue.  But we want to know first whether or
   // not we are logged in.  We might already know that from the server via cookies, but if not, find out.
   if (!loginStateKnown.value) {
