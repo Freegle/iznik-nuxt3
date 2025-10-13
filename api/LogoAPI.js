46f51c80c377478a5773e743b14a0cc4a12773fe
edwh <edward@ehibbert.org.uk>
2025-10-13 13:31:09 +0100

    Migrate logo API call from v1 to v2

--- api/LogoAPI.js
+++ api/LogoAPI.js
@@ -2,6 +2,6 @@ import BaseAPI from '@/api/BaseAPI'
 
 export default class LogoAPI extends BaseAPI {
   async fetch(params) {
-    return await this.$get('/logo', params)
+    return await this.$getv2('/logo', params)
   }
 }
