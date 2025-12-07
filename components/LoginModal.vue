6aa4f2d96e80406844545f5ee5213b7edaa1ab99
edwh <edward@ehibbert.org.uk>
2025-12-07 10:00:01 +0000

    Standardize secondary button style as green outlined.
    Update btn-secondary to use green border and text with transparent background.
    Remove duplicate signin-switch-btn styling from LoginModal.

--- components/LoginModal.vue
+++ components/LoginModal.vue
@@ -26,16 +26,17 @@
         <span class="signin-switch">
           <b-button
             v-if="signUp"
-            variant="link"
-            class="signin-switch-btn test-already-a-freegler"
+            variant="secondary"
+            size="sm"
+            class="test-already-a-freegler"
             @click.prevent="clickShowSignIn"
           >
             Log in
           </b-button>
           <b-button
             v-else
-            variant="link"
-            class="signin-switch-btn"
+            variant="secondary"
+            size="sm"
             @click.prevent="clickShowSignUp"
           >
             Join
@@ -1033,25 +1034,6 @@ $color-apple: #000000;
   font-size: 0.875rem;
   font-weight: normal;
   text-align: right;
-}
-
-.signin-switch-btn {
-  padding: 0.25rem 0.75rem;
-  font-size: 0.8125rem;
-  font-weight: 500;
-  color: $color-green--darker;
-  text-decoration: none;
-  border: 1px solid $color-green--darker;
-  background: transparent;
-  transition: all 0.15s ease-in-out;
-
-  &:hover,
-  &:focus {
-    color: $color-white;
-    border-color: $color-green--darker;
-    background: $color-green--darker;
-    text-decoration: none;
-  }
 }
 
 .signin-container {
